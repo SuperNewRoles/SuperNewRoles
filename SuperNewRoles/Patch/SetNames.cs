74f9687e6f294159ab5973c856b43662cfe5c53a
ykundesu <4476tv@gmail.com>
2022-01-28 19:23:59 +0900

    名前に色&役職を表示するように変更

--- SuperNewRoles/Patch/SetNames.cs
+++ SuperNewRoles/Patch/SetNames.cs
@@ -1,6 +1,7 @@
 ﻿using HarmonyLib;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using UnityEngine;
 
@@ -8,13 +9,76 @@ namespace SuperNewRoles.Patch
 {
     public class SetNamesClass
     {
+        private static string roleNames;
+        private static Color roleColors;
+
         public static void SetPlayerNameColor(PlayerControl p, Color color)
         {
             p.nameText.color = color;
         }
-        public static void SetPlayerName(PlayerControl p,string name)
+        public static void SetPlayerRoleInfo(PlayerControl p)
         {
-            p.nameText = (TMPro.TextMeshPro)name;
+                    Transform playerInfoTransform = p.nameText.transform.parent.FindChild("Info");
+                    TMPro.TextMeshPro playerInfo = playerInfoTransform != null ? playerInfoTransform.GetComponent<TMPro.TextMeshPro>() : null;
+                    if (playerInfo == null)
+                    {
+                        playerInfo = UnityEngine.Object.Instantiate(p.nameText, p.nameText.transform.parent);
+                        playerInfo.fontSize *= 0.75f;
+                        playerInfo.gameObject.name = "Info";
+                    }
+
+                    // Set the position every time bc it sometimes ends up in the wrong place due to camoflauge
+                    playerInfo.transform.localPosition = p.nameText.transform.localPosition + Vector3.up * 0.5f;
+
+                    PlayerVoteArea playerVoteArea = MeetingHud.Instance?.playerStates?.FirstOrDefault(x => x.TargetPlayerId == p.PlayerId);
+                    Transform meetingInfoTransform = playerVoteArea != null ? playerVoteArea.NameText.transform.parent.FindChild("Info") : null;
+                    TMPro.TextMeshPro meetingInfo = meetingInfoTransform != null ? meetingInfoTransform.GetComponent<TMPro.TextMeshPro>() : null;
+                    if (meetingInfo == null && playerVoteArea != null)
+                    {
+                        meetingInfo = UnityEngine.Object.Instantiate(playerVoteArea.NameText, playerVoteArea.NameText.transform.parent);
+                        meetingInfo.transform.localPosition += Vector3.down * 0.1f;
+                        meetingInfo.fontSize = 1.5f;
+                        meetingInfo.gameObject.name = "Info";
+                    }
+
+            // Set player name higher to align in middle
+            if (meetingInfo != null && playerVoteArea != null)
+            {
+                var playerName = playerVoteArea.NameText;
+                playerName.transform.localPosition = new Vector3(0.3384f, (0.0311f + 0.0683f), -0.1f);
+            }
+
+                    var role = p.getRole();
+            if (role == CustomRPC.RoleId.DefaultRole) {
+                if (p.Data.Role.IsImpostor) 
+                { 
+                    roleNames = "ImpostorName"; 
+                    roleColors = Roles.RoleClass.ImpostorRed; 
+                }
+                else 
+                {
+                    roleNames = "CrewMateName";
+                    roleColors = Roles.RoleClass.CrewmateWhite;
+                }
+
+            } else
+            {
+                var introdate = Intro.IntroDate.GetIntroDate(role);
+                roleNames = introdate.NameKey + "Name";
+                roleColors = introdate.color;
+            }
+                    
+                    
+                    string playerInfoText = "";
+                    string meetingInfoText = "";
+                        playerInfoText = $"{CustomOption.CustomOptions.cs(roleColors, roleNames)}";
+                        meetingInfoText = $"{CustomOption.CustomOptions.cs(roleColors, roleNames)}".Trim();
+
+
+
+                    playerInfo.text = playerInfoText;
+                    playerInfo.gameObject.SetActive(p.Visible);
+            if (meetingInfo != null) meetingInfo.text = MeetingHud.Instance.state == MeetingHud.VoteStates.Results ? "" : meetingInfoText;  p.nameText.color = roleColors;
         }
         public static void SetPlayerNameColors(PlayerControl player)
         {
@@ -24,27 +88,14 @@ namespace SuperNewRoles.Patch
         }
         public static void SetPlayerRoleNames(PlayerControl player)
         {
-            var role = player.getRole();
-            if (role == CustomRPC.RoleId.DefaultRole)
-            {
-                if (player.Data.Role.IsImpostor)
-                {
-                    SetPlayerName(player, ModTranslation.getString("ImpostorName") + "\n" + player.name);
-                } else
-                {
-                    SetPlayerName(player, ModTranslation.getString("CrewMateName") + "\n" + player.name);
-                }
-            }
-            else
-            {
-                SetPlayerName(player, ModTranslation.getString(Intro.IntroDate.GetIntroDate(role).NameKey + "Name") + "\n" + player.name);
-            }
+            SetPlayerRoleInfo(player);
         }
     }
-    [HarmonyPatch(typeof(HudManager), nameof(HudManager.Update))]
-    class HudManagerUpdatePatch
+
+    [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.FixedUpdate))]
+    class PlayerFixedUpdate
     {
-        static void Postfix(HudManager __instance)
+        public static void Postfix(PlayerControl __instance)
         {
             if (AmongUsClient.Instance.GameState != InnerNet.InnerNetClient.GameStates.Started) return;
             if (PlayerControl.LocalPlayer.Data.IsDead)
