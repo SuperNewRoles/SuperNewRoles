aa2622d552c85368bf45ccfab42dfa6072b13c9e
ykundesu <4476tv@gmail.com>
2022-01-30 11:02:18 +0900

    ゲーム終了時にリセットする処理を作成

---
+++ SuperNewRoles/Buttons/OnGameEndTimeReset.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperNewRoles.Buttons
+{
+    [HarmonyPatch(typeof(AmongUsClient), nameof(AmongUsClient.OnGameEnd))]
+    public class OnGameTimeEnd
+    {
+
+        public static void Prefix(AmongUsClient __instance, [HarmonyArgument(0)] ref EndGameResult endGameResult)
+        {
+            Roles.SpeedBooster.ResetSpeed();
+            Roles.EvilSpeedBooster.ResetSpeed();
+            Roles.Clergyman.LightOutEnd();
+        }
+    }
+}
