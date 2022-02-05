2059493a97194c0f1e6336592ae8a56dd8281e0a
ykundesu <4476tv@gmail.com>
2022-02-06 07:48:32 +0900

    死亡者プレイヤーを保存するクラスを作成

---
+++ SuperNewRoles/Patch/DeadPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperNewRoles.Patch
+{
+    public class DeadPlayer
+    {
+        public static List<DeadPlayer> deadPlayers = new List<DeadPlayer>();
+        public PlayerControl player;
+            public DateTime timeOfDeath;
+            public DeathReason deathReason;
+            public PlayerControl killerIfExisting;
+
+            public DeadPlayer(PlayerControl player, DateTime timeOfDeath, DeathReason deathReason, PlayerControl killerIfExisting)
+            {
+                this.player = player;
+                this.timeOfDeath = timeOfDeath;
+                this.deathReason = deathReason;
+                this.killerIfExisting = killerIfExisting;
+            }
+     }
+}
