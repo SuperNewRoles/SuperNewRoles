eb0334d0528a461b3448ec3712f9163dad5e2772
ykundesu <4476tv@gmail.com>
2022-02-26 09:50:14 +0900

    プリセット10まで追加

--- SuperNewRoles/CustomOption/CustomOptionDate.cs
+++ SuperNewRoles/CustomOption/CustomOptionDate.cs
@@ -17,7 +17,7 @@ namespace SuperNewRoles.CustomOption
     {
         public static string[] rates = new string[] { "0%", "10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%" };
 
-        public static string[] presets = new string[] { "preset1", "preset2", "preset3", "preset4", "preset5" };
+        public static string[] presets = new string[] { "preset1", "preset2", "preset3", "preset4", "preset5", "preset6", "preset7", "preset8", "preset9", "preset10" };
         public static CustomOption presetSelection;
 
 
