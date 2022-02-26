d6aca3ba12545fe895bae6ec526dfa6aa200a818
ykundesu <4476tv@gmail.com>
2022-02-26 20:05:14 +0900

    チャットペーストを作成

--- SuperNewRoles/Patch/ChatPaste.cs
+++ SuperNewRoles/Patch/ChatPaste.cs
@@ -1,22 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using UnityEngine;
-
+﻿using UnityEngine;
 namespace SuperNewRoles.Patch
 {
     class ChatPaste
     {
-        [HarmonyLib.HarmonyPatch(typeof(AmongUsClient),nameof(AmongUsClient.Update))]
+        [HarmonyLib.HarmonyPatch(typeof(KeyboardJoystick),nameof(KeyboardJoystick.Update))]
         class pastepatch
         {
             static void Postfix()
             {
                 if (HudManager.Instance.Chat.IsOpen)
                 {
-                    if (Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.V))
-                    { 
-                        HudManager.Instance.Chat.TextArea.text = HudManager.Instance.Chat.TextArea.text + ClipboardHelper.GetClipboardString();
+                    
+                    SuperNewRolesPlugin.Logger.LogInfo(HudManager.Instance.Chat.TextArea.text);
+                    if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.V))
+                    {
+                        HudManager.Instance.Chat.TextArea.text = HudManager.Instance.Chat.TextArea.text + GUIUtility.systemCopyBuffer;
                     }
                 }
             }
