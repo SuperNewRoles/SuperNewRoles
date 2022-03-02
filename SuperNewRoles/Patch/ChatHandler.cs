746ef8163dda7c2ade8e5de5ff96c929488ca2e1
ykundesu <4476tv@gmail.com>
2022-03-02 18:23:35 +0900

    チャットハンドラーを作成(赤ずきんが幽霊チャットを見えないように)

---
+++ SuperNewRoles/Patch/ChatHandler.cs
@@ -0,0 +1,56 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace SuperNewRoles.Patch
+{
+    [HarmonyPatch(typeof(ChatController),nameof(ChatController.AddChat))]
+    class ChatHandler
+    {
+        public static bool Prefix(ChatController __instance, [HarmonyArgument(0)] ref PlayerControl sourcePlayer, [HarmonyArgument(1)] ref string chatText)
+        {
+
+            if (!(bool)(UnityEngine.Object)sourcePlayer || !(bool)(UnityEngine.Object)PlayerControl.LocalPlayer)
+                return false;
+            GameData.PlayerInfo data1 = PlayerControl.LocalPlayer.Data;
+            GameData.PlayerInfo data2 = sourcePlayer.Data;
+            if (data2 == null || data1 == null || data2.IsDead && (!data1.IsDead || data1.PlayerId != data2.PlayerId || PlayerControl.LocalPlayer.isRole(CustomRPC.RoleId.NiceRedRidingHood) ))
+                return false;
+            if (__instance.chatBubPool.NotInUse == 0)
+                __instance.chatBubPool.ReclaimOldest();
+            ChatBubble bubble = __instance.chatBubPool.Get<ChatBubble>();
+            try
+            {
+                bubble.transform.SetParent(__instance.scroller.Inner);
+                bubble.transform.localScale = Vector3.one;
+                int num = (UnityEngine.Object)sourcePlayer == (UnityEngine.Object)PlayerControl.LocalPlayer ? 1 : 0;
+                if (num != 0)
+                    bubble.SetRight();
+                else
+                    bubble.SetLeft();
+                bool flag = (bool)(UnityEngine.Object)data1.Role && (bool)(UnityEngine.Object)data2.Role && data1.Role.NameColor == data2.Role.NameColor;
+                bool didVote = (bool)(UnityEngine.Object)MeetingHud.Instance && MeetingHud.Instance.DidVote(sourcePlayer.PlayerId);
+                bubble.SetCosmetics(data2);
+                __instance.SetChatBubbleName(bubble, data2, data2.IsDead, didVote, flag ? data2.Role.NameColor : Color.white);
+                if (SaveManager.CensorChat)
+                    chatText = BlockedWords.CensorWords(chatText);
+                bubble.SetText(chatText);
+                bubble.AlignChildren();
+                __instance.AlignAllBubbles();
+                if (!__instance.IsOpen && __instance.notificationRoutine == null)
+                   __instance.notificationRoutine = __instance.StartCoroutine(__instance.BounceDot());
+                if (num != 0)
+                    return false;
+                SoundManager.Instance.PlaySound(__instance.MessageSound, false).pitch = (float)(0.5 + (double)sourcePlayer.PlayerId / 15.0);
+            }
+            catch (Exception ex)
+            {
+                SuperNewRolesPlugin.Logger.LogError((object)ex);
+                __instance.chatBubPool.Reclaim((PoolableBehavior)bubble);
+            }
+            return false;
+        }
+    }
+}
