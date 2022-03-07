99c84d87e8d30d18a54905815cc4d78a64a60cb2
ykundesu <4476tv@gmail.com>
2022-03-07 20:03:36 +0900

    [SHR]会議ボタンの場合、通報できない問題を修正

--- SuperNewRoles/Mode/SuperHostRoles/ReportDeadBody.cs
+++ SuperNewRoles/Mode/SuperHostRoles/ReportDeadBody.cs
@@ -9,6 +9,16 @@ namespace SuperNewRoles.Mode.SuperHostRoles
     {
         public static bool ReportDeadBodyPatch(PlayerControl __instance,GameData.PlayerInfo target)
         {
+            //会議ボタンでもレポートでも起こる処理
+
+            if (target == null) {
+                //会議ボタンのみで起こる処理
+
+                return true;
+            };
+
+            //死体レポートのみで起こる処理
+
             if (__instance.isRole(CustomRPC.RoleId.Minimalist)) return RoleClass.Minimalist.UseReport;
             if (target.Object.isRole(CustomRPC.RoleId.Bait) && !RoleClass.Bait.ReportedPlayer.Contains(target.PlayerId)) return false;
             return true;
