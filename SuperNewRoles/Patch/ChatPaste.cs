75dc9a080f294470fa53307dbe47bea65b827f05
ykundesu <4476tv@gmail.com>
2022-03-30 09:24:31 +0900

    バージョン変更

--- SuperNewRoles/Patch/ChatPaste.cs
+++ SuperNewRoles/Patch/ChatPaste.cs
@@ -8,20 +8,23 @@ namespace SuperNewRoles.Patch
         {
             static void Postfix()
             {
-                if (HudManager.Instance.Chat.IsOpen)
+                if (HudManager.Instance.Chat.IsOpen && SaveManager.chatModeType == 1)
                 {
                     if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.V))
                     {
-                        HudManager.Instance.Chat.TextArea.text = HudManager.Instance.Chat.TextArea.text + GUIUtility.systemCopyBuffer;
+                        HudManager.Instance.Chat.TextArea.SetText(HudManager.Instance.Chat.TextArea.text + GUIUtility.systemCopyBuffer);
+                        HudManager.Instance.Chat.quickChatMenu.ResetGlyphs();
                     }
                     if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.X))
                     {
                         GUIUtility.systemCopyBuffer = HudManager.Instance.Chat.TextArea.text;
-                        HudManager.Instance.Chat.TextArea.text = "";
+                        HudManager.Instance.Chat.TextArea.Clear();
+                        HudManager.Instance.Chat.quickChatMenu.ResetGlyphs();
                     }
                     if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.C))
                     {
                         GUIUtility.systemCopyBuffer = HudManager.Instance.Chat.TextArea.text;
+                        HudManager.Instance.Chat.quickChatMenu.ResetGlyphs();
                     }
                 }
             }
