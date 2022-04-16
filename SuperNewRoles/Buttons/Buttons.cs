75e9e9200f83251ecf755bd96366c3c73d3c5749
sathumaimoamo <99487830+satsumaimoamo@users.noreply.github.com>
2022-04-16 15:14:31 +0900

    追加ノーマルキルボタン移動

--- SuperNewRoles/Buttons/Buttons.cs
+++ SuperNewRoles/Buttons/Buttons.cs
@@ -720,7 +720,7 @@ namespace SuperNewRoles.Buttons
                 },
                 () => { DoubralKiller.EndMeeting(); },
                 __instance.KillButton.graphic.sprite,
-                new Vector3(-2.7f, -0.06f, 0),
+                new Vector3(0, 1, 0),
                 __instance,
                 __instance.KillButton,
                 KeyCode.Q,
