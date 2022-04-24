46b08bad312d8680a4dc639684d566cc2a6f9387
sathumaimoamo <99487830+satsumaimoamo@users.noreply.github.com>
2022-04-25 02:19:05 +0900

    patchの分身編

---
+++ SuperNewRoles/Sabotage/Blizzard/Patch-Reactor.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperNewRoles.Sabotage.Blizzard
+{
+    public static class Reactor
+    {
+        public static ProgressTracker Instance;
+        [HarmonyPatch(typeof(ProgressTracker),nameof(ProgressTracker.FixedUpdate))]
+        class TaskBarPatch
+        {
+            public static void Postfix(ProgressTracker __instance)
+            {
+                Instance = __instance;
+                if (PlayerControl.GameOptions.TaskBarMode != TaskBarMode.Invisible)
+                {
+                    if (SabotageManager.thisSabotage == SabotageManager.CustomSabotage.Blizzard)
+                    {
+                        __instance.gameObject.SetActive(main.IsLocalEnd);
+                    }
+                }
+            }
+        }
+        //ここにリアクター関連を書こう
+    }
+}
