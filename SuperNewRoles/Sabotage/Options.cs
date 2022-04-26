ee498b2b33449476fc443f12d52cc895b82847b9
sathumaimoamo <99487830+satsumaimoamo@users.noreply.github.com>
2022-04-26 11:50:33 +0900

    いええええええええいSaboPatch（動作しない）を追加いたぜええ

--- SuperNewRoles/Sabotage/Options.cs
+++ SuperNewRoles/Sabotage/Options.cs
@@ -15,6 +15,10 @@ namespace SuperNewRoles.Sabotage
         public static CustomOption.CustomOption BlizzardSlowSpeedmagnificationSetting;
         public static CustomOption.CustomOption BlizzardReleaseTimeSetting;
         public static CustomOption.CustomOption BlizzardIsAllEndSabotageSetting;
+        public static CustomOption.CustomOption skeldReactorDuration;
+        public static CustomOption.CustomOption miraReactorDuration;
+        public static CustomOption.CustomOption polusReactorDuration;
+        public static CustomOption.CustomOption airshipReactorDuration;
         public static void Load()
         {
             SabotageSetting = CustomOption.CustomOption.Create(296, "SabotageSetting", false, null, isHeader: true);
@@ -23,10 +27,14 @@ namespace SuperNewRoles.Sabotage
             CognitiveDeficitOutfitUpdateTimeSetting = CustomOption.CustomOption.Create(298, "CognitiveDeficitSabotageOutfitUpdateTimeSetting", 3f,0.5f,10f,0.5f, CognitiveDeficitSetting);
             CognitiveDeficitReleaseTimeSetting = CustomOption.CustomOption.Create(299, "CognitiveDeficitSabotageReleaseTimeSetting", 3f, 0.5f, 10f, 0.5f, CognitiveDeficitSetting);
             CognitiveDeficitIsAllEndSabotageSetting = CustomOption.CustomOption.Create(300, "CognitiveDeficitSabotageIsAllPlayerEndSabotageSetting", true, CognitiveDeficitSetting);            //
-            BlizzardSetting = CustomOption.CustomOption.Create(297, "SabotageCognitiveDeficitSetting", false, SabotageSetting);
+            BlizzardSetting = CustomOption.CustomOption.Create(297, "SabotageBlizzardSetting", false, SabotageSetting);
             BlizzardSlowSpeedmagnificationSetting = CustomOption.CustomOption.Create(315, "BlizzardSlowSpeedmagnificationSetting", 3f, 0.5f, 10f, 0.5f, BlizzardSetting);
             BlizzardReleaseTimeSetting = CustomOption.CustomOption.Create(316, "BlizzardSabotageReleaseTimeSetting", 3f, 0.5f, 10f, 0.5f, BlizzardSetting);
             BlizzardIsAllEndSabotageSetting = CustomOption.CustomOption.Create(317, "BlizzardSabotageIsAllPlayerEndSabotageSetting", true, BlizzardSetting);
+            skeldReactorDuration = CustomOption.CustomOption.Create(318, "skeldReactorDuration", 60f, 0f, 600f, 1f, SabotageSetting, format: "unitSeconds");
+            miraReactorDuration = CustomOption.CustomOption.Create(319, "miraReactorDuration", 60f, 0f, 600f, 1f, SabotageSetting, format: "unitSeconds");
+            polusReactorDuration = CustomOption.CustomOption.Create(320, "polusReactorDuration", 60f, 0f, 600f, 1f, SabotageSetting, format: "unitSeconds");
+            airshipReactorDuration = CustomOption.CustomOption.Create(321, "airshipReactorDuration", 60f, 0f, 600f, 1f, SabotageSetting, format: "unitSeconds");
         }
     }
 }
