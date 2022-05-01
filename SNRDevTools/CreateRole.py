1a3d91ffb68668b48e973a7a95797a4613ecc13e
月城蔵徒 <104145991+Kurato-Tsukishiro@users.noreply.github.com>
2022-05-02 01:56:10 +0900

    220502-手動更新

--- SNRDevTools/CreateRole.py
+++ SNRDevTools/CreateRole.py
@@ -2,7 +2,7 @@ while True:
     rolename = input("Role名:")
     intronum = int(input("イントロ数:"))
     team = input("陣営(0:インポ,1:第三陣営,2:クルー):")
-    baseurl = r"C:\\Users\\wingc\\source\\repos\\Kurato-Tsukishiro\\SuperNewRoles\\SuperNewRoles\\"
+    baseurl = r"..\\SuperNewRoles\\"
     if team=="0":
         color ="ImpostorRed"
     else:
