93a432e9ce0949f25022427a21a12591483fcc29
Hebepuni <HebePuni@users.noreply.github.com>
2022-05-13 21:20:11 +0900

    [SHR] ・スターの名前表示が崩れる（別の名前にコロコロ変わる()）バグを修正しました

--- SuperNewRoles/Mode/SuperHostRoles/FixedUpdate.cs
+++ SuperNewRoles/Mode/SuperHostRoles/FixedUpdate.cs
@@ -89,16 +89,22 @@ namespace SuperNewRoles.Mode.SuperHostRoles
                     {
                         if (RoleClass.Celebrity.ChangeRoleView)
                         {
-                            foreach (PlayerControl Celebrity in RoleClass.Celebrity.ViewPlayers)
+                            foreach (PlayerControl p2 in RoleClass.Celebrity.ViewPlayers)
                             {
-                                Celebrity.RpcSetNamePrivate(ModHelpers.cs(RoleClass.Celebrity.color, p.getDefaultName()));
+                                if( p.PlayerId != p2.PlayerId )
+                                {
+                                    p2.RpcSetNamePrivate(ModHelpers.cs(RoleClass.Celebrity.color, p2.getDefaultName()), p);
+                                }
                             }
                         }
                         else
                         {
-                            foreach (PlayerControl Celebrity in RoleClass.Celebrity.CelebrityPlayer)
+                            foreach (PlayerControl p2 in RoleClass.Celebrity.CelebrityPlayer)
                             {
-                                Celebrity.RpcSetNamePrivate(ModHelpers.cs(RoleClass.Celebrity.color, p.getDefaultName()),p);
+                                if( p.PlayerId != p2.PlayerId )
+                                {
+                                    p2.RpcSetNamePrivate(ModHelpers.cs(RoleClass.Celebrity.color, p2.getDefaultName()), p);
+                                }
                             }
                         }
                         bool IsMadmateCheck = Madmate.CheckImpostor(p);
