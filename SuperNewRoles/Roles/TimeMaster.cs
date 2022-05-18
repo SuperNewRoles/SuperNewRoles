a2ab97c0b122b65a75c12b0af53988c74f5e8f17
月城蔵徒 <104145991+Kurato-Tsukishiro@users.noreply.github.com>
2022-05-18 14:34:58 +0900

    220518-TimeMaster_3-TimeMaster.csの更新

--- SuperNewRoles/Roles/TimeMaster.cs
+++ SuperNewRoles/Roles/TimeMaster.cs
@@ -1,51 +1,17 @@
-using HarmonyLib;
-using Hazel;
-using System;
-using SuperNewRoles.Patches;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+
 using SuperNewRoles.Buttons;
-using SuperNewRoles.CustomOption;
 
 namespace SuperNewRoles.Roles
 {
     class TimeMaster
     {
-
-        public static void ResetCoolDown()
+        public static void ResetTimeMasterButton()
         {
-            HudManagerStartPatch.TimeMasterTimeMasterShieldButton.MaxTimer = RoleClass.TimeMaster.Cooldown;
-            RoleClass.TimeMaster.ButtonTimer = DateTime.Now;
-        }
-        public static bool IsTimeMaster(PlayerControl Player)
-        {
-            if (RoleClass.TimeMaster.TimeMasterPlayer.IsCheckListPlayerControl(Player))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        public static void TimeShieldStart()
-        {
-            RoleClass.TimeMaster.ShieldActive = true;
-            CustomRPC.RPCProcedure.TimeMasterShield();
-        }
-
-        public static void TimeShieldEnd()
-        {
-            if (!RoleClass.TimeMaster.ShieldActive) return;
-            RoleClass.TimeMaster.ShieldActive = false;
-        }
-        public static void EndMeeting()
-        {
-            HudManagerStartPatch.TimeMasterTimeMasterShieldButton.MaxTimer = RoleClass.TimeMaster.Cooldown;
-            RoleClass.TimeMaster.ButtonTimer = DateTime.Now;
-            RoleClass.TimeMaster.ShieldActive = false;
+            HudManagerStartPatch.TimeMasterTimeMasterShieldButton.Timer = HudManagerStartPatch.TimeMasterTimeMasterShieldButton.MaxTimer;
             HudManagerStartPatch.TimeMasterTimeMasterShieldButton.isEffectActive = false;
+            HudManagerStartPatch.TimeMasterTimeMasterShieldButton.actionButton.cooldownTimerText.color = Palette.EnabledColor;
         }
+
     }
 }
+
