a927a366e362e57278ef8a06821d2f139f863299
ykundesu <4476tv@gmail.com>
2022-05-21 14:55:41 +0900

    WrapUpを一箇所へ集約

--- SuperNewRoles/Roles/EvilGambler.cs
+++ SuperNewRoles/Roles/EvilGambler.cs
@@ -10,41 +10,6 @@ namespace SuperNewRoles.Roles
 {
     class EvilGambler
     {
-        [HarmonyPatch(typeof(ExileController), nameof(ExileController.WrapUp))]
-        public class CheckEndGamePatch
-        {
-            public static void Postfix(ExileController __instance)
-            {
-                try
-                {
-                    endm();
-                }
-                catch (Exception e)
-                {
-                    SuperNewRolesPlugin.Logger.LogInfo("EvilGambler:" + e);
-                }
-            }
-        }
-        [HarmonyPatch(typeof(AirshipExileController), nameof(AirshipExileController.WrapUpAndSpawn))]
-        public class CheckAirShipEndGamePatch
-        {
-            public static void Postfix(AirshipExileController __instance)
-            {
-                try
-                {
-                    endm();
-                }
-                catch (Exception e)
-                {
-                    SuperNewRolesPlugin.Logger.LogInfo("EvilGambler:" + e);
-                }
-            }
-        }
-        public static void endm() {
-            //HudManager.Instance.KillButton.SetCoolDown(EvilGamblerMurder.temp, EvilGamblerMurder.temp);
-            //PlayerControl.LocalPlayer.SetKillTimer(EvilGamblerMurder.temp);
-        }
-
         public static class EvilGamblerMurder
         {
             public static void Prefix(PlayerControl __instance, [HarmonyArgument(0)] PlayerControl target)
--- SuperNewRoles/Roles/Seer.cs
+++ SuperNewRoles/Roles/Seer.cs
@@ -33,25 +33,8 @@ namespace SuperNewRoles.Roles
 
         public static class ExileControllerWrapUpPatch
         {
-            [HarmonyPatch(typeof(ExileController), nameof(ExileController.WrapUp))]
-            class BaseExileControllerPatch
-            {
-                public static void Postfix(ExileController __instance)
-                {
-                    WrapUpPostfix(__instance.exiled);
-                }
-            }
 
-            [HarmonyPatch(typeof(AirshipExileController), nameof(AirshipExileController.WrapUpAndSpawn))]
-            class AirshipExileControllerPatch
-            {
-                public static void Postfix(AirshipExileController __instance)
-                {
-                    WrapUpPostfix(__instance.exiled);
-                }
-            }
-
-            static void WrapUpPostfix(GameData.PlayerInfo exiled)
+            public static void WrapUpPostfix(GameData.PlayerInfo exiled)
             {
                 var role = PlayerControl.LocalPlayer.getRole();
                 if (role == RoleId.Seer || role == RoleId.MadSeer || role == RoleId.EvilSeer)
