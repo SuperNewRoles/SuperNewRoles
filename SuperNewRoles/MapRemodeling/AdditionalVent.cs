72a5b2b35e6b42f0b710b45b72796092fb56ebf6
satsumaimoamo <meiabao25@gmail.com>
2022-06-14 01:14:10 +0900

    選んでなくても使われる問題を修正

--- SuperNewRoles/MapRemodeling/AdditionalVent.cs
+++ SuperNewRoles/MapRemodeling/AdditionalVent.cs
@@ -45,7 +45,7 @@ namespace SuperNewRoles.MapRemodeling
             System.Console.WriteLine("AddAdditionalVents");
 
             //MiraHQにベントを追加する
-            if (PlayerControl.GameOptions.MapId == 1 && MapOptions.MapOption.MiraAdditionalVents.getBool()　&& ModeHandler.isMode(ModeId.Default))
+            if (PlayerControl.GameOptions.MapId == 1 && MapOptions.MapOption.MiraAdditionalVents.getBool() && ModeHandler.isMode(ModeId.Default) && MapOption.MapOptionSetting.getBool() && MapOption.MapRemodelingOption.getBool())
             {
                 AdditionalVents vents1 = new(new Vector3(11.3518f, 10.4786f, PlayerControl.LocalPlayer.transform.position.z + 1f)); // 研究室
                 AdditionalVents vents2 = new(new Vector3(12.1288f, 7.2f, PlayerControl.LocalPlayer.transform.position.z + 1f)); // Y字下
@@ -59,7 +59,7 @@ namespace SuperNewRoles.MapRemodeling
             }
 
             // Polusにベントを追加する
-            if (PlayerControl.GameOptions.MapId == 2 && MapOptions.MapOption.PolusAdditionalVents.getBool()　&& ModeHandler.isMode(ModeId.Default))
+            if (PlayerControl.GameOptions.MapId == 2 && MapOptions.MapOption.PolusAdditionalVents.getBool() && ModeHandler.isMode(ModeId.Default) && MapOption.MapOptionSetting.getBool() && MapOption.MapRemodelingOption.getBool())
             {
                 AdditionalVents vents1 = new(new Vector3(36.54f, -21.77f, PlayerControl.LocalPlayer.transform.position.z + 1f)); // 標本室
                 AdditionalVents vents2 = new(new Vector3(11.5522f, -21.1158f, PlayerControl.LocalPlayer.transform.position.z + 1f)); // ウェポン
@@ -73,7 +73,7 @@ namespace SuperNewRoles.MapRemodeling
             }
 
             // AirShipにベントを追加する
-            if (PlayerControl.GameOptions.MapId == 4 && MapOptions.MapOption.AirShipAdditionalVents.getBool()　&& ModeHandler.isMode(ModeId.Default))
+            if (PlayerControl.GameOptions.MapId == 4 && MapOptions.MapOption.AirShipAdditionalVents.getBool() && ModeHandler.isMode(ModeId.Default) && MapOption.MapOptionSetting.getBool() && MapOption.MapRemodelingOption.getBool())
             {
                 SuperNewRolesPlugin.Logger.LogInfo("べんとおおおお");
                 AdditionalVents vents1 = new(new Vector3(23.5483f, -5.589f, PlayerControl.LocalPlayer.transform.position.z + 1f)); // 診察室
