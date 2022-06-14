af94bddb820a83c8f100901a979660d16ec50136
satsumaimoamo <meiabao25@gmail.com>
2022-06-14 21:17:32 +0900

    とりあえず表示

--- SuperNewRoles/Roles/Conjurer.cs
+++ SuperNewRoles/Roles/Conjurer.cs
@@ -85,7 +85,7 @@ namespace SuperNewRoles.Roles
             if (boxAnimationSprites == null || boxAnimationSprites.Length == 0) return null;
             index = Mathf.Clamp(index, 0, boxAnimationSprites.Length - 1);
             if (boxAnimationSprites[index] == null)
-                boxAnimationSprites[index] = ModHelpers.loadSpriteFromResources($"TheOtherRoles.Resources.TricksterAnimation.trickster_box_00{index + 1:00}.png", 175f);
+                boxAnimationSprites[index] = ModHelpers.loadSpriteFromResources($"SuperNewRoles.Resources.ConjurerMarker.png", 175f);
             return boxAnimationSprites[index];
         }
 
