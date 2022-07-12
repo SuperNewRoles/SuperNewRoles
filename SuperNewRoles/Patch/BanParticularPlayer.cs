889ff06e9063e6021a2249c620d3dfd77ad56edd
じゅきまぐろ <coountry05300727@gmail.com>
2022-07-12 18:34:12 +0900

    謎のコードを少し修正

--- SuperNewRoles/Patch/BanParticularPlayer.cs
+++ SuperNewRoles/Patch/BanParticularPlayer.cs
@@ -8,11 +8,10 @@ namespace SuperNewRoles.Patch
     {
         public static void Postfix(AmongUsClient __instance, [HarmonyArgument(0)] ClientData client)
         {
-            Logger.Info($"{client.PlayerName}(ClientID:{client.Id})が参加", "Session");
+            SuperNewRolesPlugin.Logger.LogInfo($"{client.PlayerName}(ClientID:{client.Id})が参加");
             if (DestroyableSingleton<FriendsListManager>.Instance.IsPlayerBlockedUsername(client.FriendCode) && AmongUsClient.Instance.AmHost)
             {
                 AmongUsClient.Instance.KickPlayer(client.Id, true);
-                Logger.Info($"ブロックしているプレイヤー{client?.PlayerName}({client.FriendCode})をBANしました。", "BAN");
                 SuperNewRolesPlugin.Logger.LogInfo($"ブロックされているプレイヤー{client?.PlayerName}({client.FriendCode})をBANしました");
             }
         }
