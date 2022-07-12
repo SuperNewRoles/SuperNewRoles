8345adc0bc9d125db6fa5dc59d2c9d31616280b9
じゅきまぐろ <coountry05300727@gmail.com>
2022-07-12 18:23:38 +0900

    フレンド機能でブロックしているプレイヤーを自動でBANする機能を追加

---
+++ SuperNewRoles/Patch/BanParticularPlayer.cs
@@ -0,0 +1,19 @@
+using HarmonyLib;
+using InnerNet;
+
+namespace SuperNewRoles.Patch
+{
+    [HarmonyPatch(typeof(AmongUsClient), nameof(AmongUsClient.OnPlayerJoined))]
+    class OnPlayerJoinedPatch
+    {
+        public static void Postfix(AmongUsClient __instance, [HarmonyArgument(0)] ClientData client)
+        {
+            Logger.Info($"{client.PlayerName}(ClientID:{client.Id})が参加", "Session");
+            if (DestroyableSingleton<FriendsListManager>.Instance.IsPlayerBlockedUsername(client.FriendCode) && AmongUsClient.Instance.AmHost)
+            {
+                AmongUsClient.Instance.KickPlayer(client.Id, true);
+                Logger.Info($"ブロックしているプレイヤー{client?.PlayerName}({client.FriendCode})をBANしました。", "BAN");
+            }
+        }
+    }
+}
