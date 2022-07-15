26a95d0560c645d6f3488dce4bb08933d7ea25b5
月城蔵徒 <104145991+Kurato-Tsukishiro@users.noreply.github.com>
2022-07-16 01:34:15 +0900

    トイレファンが一人以上に設定できなかった問題の修正

--- SuperNewRoles/AllRoleSetClass.cs
+++ SuperNewRoles/AllRoleSetClass.cs
@@ -852,6 +852,7 @@ namespace SuperNewRoles
                 RoleId.Smasher => CustomOptions.SmasherPlayerCount.getFloat(),
                 RoleId.SuicideWisher => CustomOptions.SuicideWisherPlayerCount.getFloat(),
                 RoleId.Neet => CustomOptions.NeetPlayerCount.getFloat(),
+                RoleId.ToiletFan => CustomOptions.NeetPlayerCount.getFloat(),
                 //プレイヤーカウント
                 _ => 1,
             };
