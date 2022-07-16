fc7d63387412bbe0a1834d0aa68dd727f153a7a1
satsumaimoamo <meiabao25@gmail.com>
2022-07-16 11:43:55 +0900

    idの重複を修正

--- SuperNewRoles/CustomOption/CustomOptionDate.cs
+++ SuperNewRoles/CustomOption/CustomOptionDate.cs
@@ -1371,8 +1371,8 @@ namespace SuperNewRoles.CustomOption
             ToiletFanPlayerCount = CustomOption.Create(657, true, CustomOptionType.Crewmate, "SettingPlayerCountName", CrewPlayers[0], CrewPlayers[1], CrewPlayers[2], CrewPlayers[3], ToiletFanOption);
             ToiletFanCoolTime = CustomOption.Create(658, true, CustomOptionType.Crewmate, "ToiletCoolDownSetting", 30f, 0f, 60f, 2.5f, ToiletFanOption);
 
-            AllOpenerOption = new CustomRoleOption(674, true, CustomOptionType.Crewmate, "AllOpenerName", RoleClass.AllOpener.color, 1);
-            AllOpenerPlayerCount = CustomOption.Create(675, true, CustomOptionType.Crewmate, "SettingPlayerCountName", CrewPlayers[0], CrewPlayers[1], CrewPlayers[2], CrewPlayers[3], AllOpenerOption);
+            AllOpenerOption = new CustomRoleOption(682, true, CustomOptionType.Crewmate, "AllOpenerName", RoleClass.AllOpener.color, 1);
+            AllOpenerPlayerCount = CustomOption.Create(683, true, CustomOptionType.Crewmate, "SettingPlayerCountName", CrewPlayers[0], CrewPlayers[1], CrewPlayers[2], CrewPlayers[3], AllOpenerOption);
             //表示設定
 
             QuarreledOption = CustomOption.Create(432, true, CustomOptionType.Neutral, cs(RoleClass.Quarreled.color, "QuarreledName"), false, null, isHeader: true);
