5543f2a1014dfda438eb251e7d876874751dfd37
ykundesu <4476tv@gmail.com>
2022-08-11 08:26:05 +0900

    残りCustomOption作成&翻訳のみ

--- SuperNewRoles/Patch/TaskCount.cs
+++ SuperNewRoles/Patch/TaskCount.cs
@@ -1,11 +1,36 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using HarmonyLib;
+using SuperNewRoles.CustomOption;
 using SuperNewRoles.Mode;
 
 namespace SuperNewRoles.Patch
 {
     class TaskCount
     {
+        [HarmonyPatch(typeof(NormalPlayerTask), nameof(NormalPlayerTask.PickRandomConsoles))]
+        class NormalPlayerTaskPickRandomConsolesPatch
+        {
+            private static int numWireTask => CustomOptions.WireTaskNum.GetInt();
+            static void Postfix(NormalPlayerTask __instance, TaskTypes taskType, byte[] consoleIds)
+            {
+                if (taskType != TaskTypes.FixWiring || !CustomOptions.WireTaskIsRandom.getBool()) return;
+                List<Console> orgList = ShipStatus.Instance.AllConsoles.Where((global::Console t) => t.TaskTypes.Contains(taskType)).ToList<global::Console>();
+                List<Console> list = new(orgList);
+
+                __instance.MaxStep = numWireTask;
+                __instance.Data = new byte[numWireTask];
+                for (int i = 0; i < __instance.Data.Length; i++)
+                {
+                    if (list.Count == 0)
+                        list = new List<Console>(orgList);
+                    int index = ModHelpers.GetRandomIndex(list);
+                    __instance.Data[i] = (byte)list[index].ConsoleId;
+                    list.RemoveAt(index);
+                }
+            }
+        }
         [HarmonyPatch(typeof(NormalPlayerTask), nameof(NormalPlayerTask.FixedUpdate))]
         public static class NormalPlayerTaskPatch
         {
