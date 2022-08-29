d5bf01ad3ae4af8f84ba7a3d56e7e3b8594768ee
月城蔵徒 <104145991+Kurato-Tsukishiro@users.noreply.github.com>
2022-08-30 02:09:19 +0900

    指摘点の修正(翻訳対応&文字列補完)

--- SuperNewRoles/Patch/ChatCommand.cs
+++ SuperNewRoles/Patch/ChatCommand.cs
@@ -71,8 +71,8 @@ namespace SuperNewRoles.Patch
                         else //ゲスト時には使用不可能にする
                         {
                             handled = true;
-                            __instance.AddChat(PlayerControl.LocalPlayer, $"名前を変更することができません。\n/renameコマンドはホスト時のみ\n使用可能です。");
-                            SuperNewRolesPlugin.Logger.LogWarning("ホストでない時に" + text + "を使用しました。ホストでない時は/renameは使用できません。");
+                            __instance.AddChat(PlayerControl.LocalPlayer,ModTranslation.GetString("CannotUseRenameMessage"));
+                            SuperNewRolesPlugin.Logger.LogWarning($"ホストでない時に{text}を使用しました。ホストでない時は/renameは使用できません。");
                         }
                     }
 
