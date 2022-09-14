0cd93ff557c8fe859898e9290e2fc8695f784e14
Sakurander <sakurander77@gmail.com>
2022-09-14 20:32:02 +0900

    PlayerControlにSetOutlineを追加

--- SuperNewRoles/Patch/PlayerControlPatch.cs
+++ SuperNewRoles/Patch/PlayerControlPatch.cs
@@ -1063,5 +1063,12 @@ namespace SuperNewRoles.Patches
             }
             return result;
         }
+        public static void SetPlayerOutline(PlayerControl target, Color color)
+        {
+            if (target == null || target.MyRend == null) return;
+
+            target.MyRend().material.SetFloat("_Outline", 1f);
+            target.MyRend().material.SetColor("_OutlineColor", color);
+        }
     }
 }
