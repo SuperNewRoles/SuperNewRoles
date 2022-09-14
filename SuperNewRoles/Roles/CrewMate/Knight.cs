e00c15ab77bae9f92b8fa68d8aee149fa79195ae
月城蔵徒 <104145991+Kurato-Tsukishiro@users.noreply.github.com>
2022-09-14 14:45:05 +0900

    knightのCustomOptionsとRoleClassの作成(初期設計段階で必要だと分かった物の)

--- SuperNewRoles/Roles/CrewMate/Knight.cs
+++ SuperNewRoles/Roles/CrewMate/Knight.cs
@@ -12,20 +12,31 @@ namespace SuperNewRoles.Roles.CrewMate
         // CustomOptionDate
         public static CustomRoleOption KnightOption;
         public static CustomOption KnightPlayerCount;
-
+        public static CustomOption KnightCanAnnounceOfProtected;
+        public static CustomOption KnightSetTheUpperLimitOfTheGuarding;
+        public static CustomOption KnightMaximumNumberOfTimes;
         public static void SetupCustomOptions()
         {
             KnightOption = new(OptionId, false, CustomOptionType.Crewmate, "KnightName", color, 1);
             KnightPlayerCount = CustomOption.Create(OptionId + 1, false, CustomOptionType.Crewmate, "SettingPlayerCountName", CrewPlayers[0], CrewPlayers[1], CrewPlayers[2], CrewPlayers[3], KnightOption);
+            KnightCanAnnounceOfProtected = CustomOption.Create(OptionId + 2, false, CustomOptionType.Crewmate, "CanAnnounceOfProtected", true, KnightOption);
+            KnightSetTheUpperLimitOfTheGuarding = CustomOption.Create(OptionId + 3, false, CustomOptionType.Crewmate, "SetTheUpperLimitOfTheGuarding", false, KnightOption);
+            KnightMaximumNumberOfTimes = CustomOption.Create(OptionId + 4, false, CustomOptionType.Crewmate, "MaximumNumberOfTimes", 5f, 0f, 30f, 1f, KnightSetTheUpperLimitOfTheGuarding);
         }
 
         // RoleClass
         public static List<PlayerControl> Player;
         public static Color32 color = new(229, 228, 230, byte.MaxValue);
+        public static bool CanProtect;
+        public static float Times;
 
         public static void ClearAndReload()
         {
             Player = new();
+            Times = KnightMaximumNumberOfTimes.GetFloat(); //最大守護回数の取得
+            CanProtect = true; //守護を使用可能か
+
+            //CustomOptionからのGetBool()は判定が必要な場所でその都度行う為、ここに入れない。
         }
     }
 }
