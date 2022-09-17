0b4220216d8d8cfc41199c2d7879fd228bb22915
Sakurander <sakurander77@gmail.com>
2022-09-17 17:55:25 +0900

    ローカル変数を介さず直接辞書に追加するように

--- SuperNewRoles/CustomCosmetics/CustomColors.cs
+++ SuperNewRoles/CustomCosmetics/CustomColors.cs
@@ -68,128 +68,47 @@ namespace SuperNewRoles.CustomCosmetics
             List<Color32> shadowlist = Enumerable.ToList<Color32>(Palette.ShadowColors);
 
 
-            Color32[] salmon = { new(239, 191, 192, byte.MaxValue), new(182, 119, 114, byte.MaxValue) };
-            LighterColorData.Add(ColorType.Salmon, salmon);
-
-            Color32[] bordeaux = { new(109, 7, 26, byte.MaxValue), new(54, 2, 11, byte.MaxValue) };
-            NoLighterColorData.Add(ColorType.Bordeaux, bordeaux);
-
-            Color32[] olive = { new(154, 140, 61, byte.MaxValue), new(104, 95, 40, byte.MaxValue) };
-            NoLighterColorData.Add(ColorType.Olive, olive);
-
-            Color32[] turqoise = { new(22, 132, 176, byte.MaxValue), new(15, 89, 117, byte.MaxValue) };
-            NoLighterColorData.Add(ColorType.Turqoise, turqoise);
-
-            Color32[] mint = { new(111, 192, 156, byte.MaxValue), new(65, 148, 111, byte.MaxValue) };
-            LighterColorData.Add(ColorType.Mint, mint);
-
-            Color32[] lavender = { new(173, 126, 201, byte.MaxValue), new(131, 58, 203, byte.MaxValue) };
-            LighterColorData.Add(ColorType.Lavender, lavender);
-
-            Color32[] nougat = { new(160, 101, 56, byte.MaxValue), new(115, 15, 78, byte.MaxValue) };
-            NoLighterColorData.Add(ColorType.Nougat, nougat);
-
-            Color32[] peach = { new(255, 164, 119, byte.MaxValue), new(238, 128, 100, byte.MaxValue) };
-            LighterColorData.Add(ColorType.Peach, peach);
-
-            Color32[] wasabi = { new(112, 143, 46, byte.MaxValue), new(72, 92, 29, byte.MaxValue) };
-            NoLighterColorData.Add(ColorType.Wasabi, wasabi);
-
-            Color32[] hotPink = { new(255, 51, 102, byte.MaxValue), new(232, 0, 58, byte.MaxValue) };
-            LighterColorData.Add(ColorType.HotPink, hotPink);
-
-            Color32[] petrol = { new(0, 99, 105, byte.MaxValue), new(0, 61, 54, byte.MaxValue) };
-            LighterColorData.Add(ColorType.Petrol, petrol);
-
-            Color32[] lemon = { new(0xDB, 0xFD, 0x2F, byte.MaxValue), new(0x74, 0xE5, 0x10, byte.MaxValue) };
-            LighterColorData.Add(ColorType.Lemon, lemon);
-
-            Color32[] signalOrange = { new(0xF7, 0x44, 0x17, byte.MaxValue), new(0x9B, 0x2E, 0x0F, byte.MaxValue) };
-            LighterColorData.Add(ColorType.SignalOrange, signalOrange);
-
-            Color32[] teal = { new(0x25, 0xB8, 0xBF, byte.MaxValue), new(0x12, 0x89, 0x86, byte.MaxValue) };
-            NoLighterColorData.Add(ColorType.Teal, teal);
-
-            Color32[] blurple = { new(0x59, 0x3C, 0xD6, byte.MaxValue), new(0x29, 0x17, 0x96, byte.MaxValue) };
-            NoLighterColorData.Add(ColorType.Blurple, blurple);
-
-            Color32[] sunrise = { new(0xFF, 0xCA, 0x19, byte.MaxValue), new(0xDB, 0x44, 0x42, byte.MaxValue) };
-            LighterColorData.Add(ColorType.Sunrise, sunrise);
-
-            Color32[] ice = { new(0xA8, 0xDF, 0xFF, byte.MaxValue), new(0x59, 0x9F, 0xC8, byte.MaxValue) };
-            LighterColorData.Add(ColorType.Ice, ice);
-
-            Color32[] pitchBlack = { new(0, 0, 0, byte.MaxValue), new(0, 0, 0, byte.MaxValue) };
-            NoLighterColorData.Add(ColorType.PitchBlack, pitchBlack);
-
-            Color32[] darkmagenta = { new(139, 0, 139, byte.MaxValue), new(153, 50, 204, byte.MaxValue) };
-            NoLighterColorData.Add(ColorType.Darkmagenta, darkmagenta);
-
-            Color32[] mintcream = { new(245, 255, 250, byte.MaxValue), new(224, 255, 255, byte.MaxValue) };
-            LighterColorData.Add(ColorType.Mintcream, mintcream);
-
-            Color32[] leaf = { new(62, 90, 11, byte.MaxValue), new(34, 50, 6, byte.MaxValue) };
-            NoLighterColorData.Add(ColorType.Leaf, leaf);
-
-            Color32[] emerald = { new(98, 214, 133, byte.MaxValue), new(82, 179, 111, byte.MaxValue) };
-            LighterColorData.Add(ColorType.Emerald, emerald);
-
-            Color32[] brightyellow = { new(248, 181, 0, byte.MaxValue), new(255, 102, 0, byte.MaxValue) };
-            LighterColorData.Add(ColorType.Brightyellow, brightyellow);
-
-            Color32[] darkaqua = { new(14, 104, 188, byte.MaxValue), new(11, 85, 153, byte.MaxValue) };
-            LighterColorData.Add(ColorType.Darkaqua, darkaqua);
-
-            Color32[] matcha = { new(52, 99, 23, byte.MaxValue), new(34, 54, 19, byte.MaxValue) };
-            NoLighterColorData.Add(ColorType.Matcha, matcha);
-
-            Color32[] pitchwhite = { new(255, 255, 255, byte.MaxValue), new(240, 240, 240, byte.MaxValue) };
-            LighterColorData.Add(ColorType.Pitchwhite, pitchwhite);
-
-            Color32[] darkSky = { new(64, 128, 192, byte.MaxValue), new(32, 96, 128, byte.MaxValue) };
-            LighterColorData.Add(ColorType.Darksky, darkSky);
-
-            Color32[] intenseblue = { new(83, 136, 255, byte.MaxValue), new(76, 122, 230, byte.MaxValue) };
-            LighterColorData.Add(ColorType.Intenseblue, intenseblue);
-
-            Color32[] blueclosertoblack = { new(0, 0, 50, byte.MaxValue), new(0, 0, 25, byte.MaxValue) };
-            NoLighterColorData.Add(ColorType.Blueclosertoblack, blueclosertoblack);
-
-            Color32[] sunkengreenishblue = { new(128, 156, 166, byte.MaxValue), new(115, 141, 153, byte.MaxValue) };
-            LighterColorData.Add(ColorType.Sunkengreenishblue, sunkengreenishblue);
-
-            Color32[] azi = { new(100, 48, 0, byte.MaxValue), new(98, 5, 0, byte.MaxValue) };
-            NoLighterColorData.Add(ColorType.Azi, azi);
-
-            Color32[] pitchred = { new(255, 0, 0, byte.MaxValue), new(220, 20, 60, byte.MaxValue) };
-            LighterColorData.Add(ColorType.Pitchred, pitchred);
-
-            Color32[] pitchblue = { new(0, 0, 128, byte.MaxValue), new(0, 0, 112, byte.MaxValue) };
-            LighterColorData.Add(ColorType.Pitchblue, pitchblue);
-
-            Color32[] pitchgreen = { new(0, 128, 0, byte.MaxValue), new(0, 120, 0, byte.MaxValue) };
-            LighterColorData.Add(ColorType.Pitchgreen, pitchgreen);
-
-            Color32[] pitchyellow = { new(255, 255, 0, byte.MaxValue), new(255, 235, 0, byte.MaxValue) };
-            LighterColorData.Add(ColorType.Pitchyellow, pitchyellow);
-
-            Color32[] backblue = { new(0, 128, 255, byte.MaxValue), new(0, 85, 255, byte.MaxValue) };
-            LighterColorData.Add(ColorType.Backblue, backblue);
-
-            Color32[] mildpurple = { new(109, 83, 131, byte.MaxValue), new(109, 83, 131, byte.MaxValue) };
-            LighterColorData.Add(ColorType.Mildpurple, mildpurple);
-
-            Color32[] ashishreddishpurplecolor = { new(139, 102, 118, byte.MaxValue), new(139, 102, 118, byte.MaxValue) };
-            NoLighterColorData.Add(ColorType.Ashishreddishpurplecolor, ashishreddishpurplecolor);
-
-            Color32[] melon = { new(0, 225, 129, byte.MaxValue), new(24, 255, 81, byte.MaxValue) };
-            LighterColorData.Add(ColorType.Melon, melon);
-
-            Color32[] crasyublue = { new(2, 38, 106, byte.MaxValue), new(64, 0, 111, byte.MaxValue) };
-            NoLighterColorData.Add(ColorType.Crasyublue, crasyublue);
-
-            Color32[] lightgreen = { new(226, 255, 5, byte.MaxValue), new(192, 201, 10, byte.MaxValue) };
-            LighterColorData.Add(ColorType.Lightgreen, lightgreen);
+            LighterColorData.Add(ColorType.Salmon, new Color32[] { new(239, 191, 192, byte.MaxValue), new(182, 119, 114, byte.MaxValue) });
+            NoLighterColorData.Add(ColorType.Bordeaux, new Color32[] { new(109, 7, 26, byte.MaxValue), new(54, 2, 11, byte.MaxValue) });
+            NoLighterColorData.Add(ColorType.Olive, new Color32[] { new(154, 140, 61, byte.MaxValue), new(104, 95, 40, byte.MaxValue) });
+            NoLighterColorData.Add(ColorType.Turqoise, new Color32[] { new(22, 132, 176, byte.MaxValue), new(15, 89, 117, byte.MaxValue) });
+            LighterColorData.Add(ColorType.Mint, new Color32[] { new(111, 192, 156, byte.MaxValue), new(65, 148, 111, byte.MaxValue) });
+            LighterColorData.Add(ColorType.Lavender, new Color32[] { new(173, 126, 201, byte.MaxValue), new(131, 58, 203, byte.MaxValue) });
+            NoLighterColorData.Add(ColorType.Nougat, new Color32[] { new(160, 101, 56, byte.MaxValue), new(115, 15, 78, byte.MaxValue) });
+            LighterColorData.Add(ColorType.Peach, new Color32[] { new(255, 164, 119, byte.MaxValue), new(238, 128, 100, byte.MaxValue) });
+            NoLighterColorData.Add(ColorType.Wasabi, new Color32[] { new(112, 143, 46, byte.MaxValue), new(72, 92, 29, byte.MaxValue) });
+            LighterColorData.Add(ColorType.HotPink, new Color32[] { new(255, 51, 102, byte.MaxValue), new(232, 0, 58, byte.MaxValue) });
+            LighterColorData.Add(ColorType.Petrol, new Color32[] { new(0, 99, 105, byte.MaxValue), new(0, 61, 54, byte.MaxValue) });
+            LighterColorData.Add(ColorType.Lemon, new Color32[] { new(0xDB, 0xFD, 0x2F, byte.MaxValue), new(0x74, 0xE5, 0x10, byte.MaxValue) });
+            LighterColorData.Add(ColorType.SignalOrange, new Color32[] { new(0xF7, 0x44, 0x17, byte.MaxValue), new(0x9B, 0x2E, 0x0F, byte.MaxValue) });
+            NoLighterColorData.Add(ColorType.Teal, new Color32[] { new(0x25, 0xB8, 0xBF, byte.MaxValue), new(0x12, 0x89, 0x86, byte.MaxValue) });
+            NoLighterColorData.Add(ColorType.Blurple, new Color32[] { new(0x59, 0x3C, 0xD6, byte.MaxValue), new(0x29, 0x17, 0x96, byte.MaxValue) });
+            LighterColorData.Add(ColorType.Sunrise, new Color32[] { new(0xFF, 0xCA, 0x19, byte.MaxValue), new(0xDB, 0x44, 0x42, byte.MaxValue) });
+            LighterColorData.Add(ColorType.Ice, new Color32[] { new(0xA8, 0xDF, 0xFF, byte.MaxValue), new(0x59, 0x9F, 0xC8, byte.MaxValue) });
+            NoLighterColorData.Add(ColorType.PitchBlack, new Color32[] { new(0, 0, 0, byte.MaxValue), new(0, 0, 0, byte.MaxValue) });
+            NoLighterColorData.Add(ColorType.Darkmagenta, new Color32[] { new(139, 0, 139, byte.MaxValue), new(153, 50, 204, byte.MaxValue) });
+            LighterColorData.Add(ColorType.Mintcream, new Color32[] { new(245, 255, 250, byte.MaxValue), new(224, 255, 255, byte.MaxValue) });
+            NoLighterColorData.Add(ColorType.Leaf, new Color32[] { new(62, 90, 11, byte.MaxValue), new(34, 50, 6, byte.MaxValue) });
+            LighterColorData.Add(ColorType.Emerald, new Color32[] { new(98, 214, 133, byte.MaxValue), new(82, 179, 111, byte.MaxValue) });
+            LighterColorData.Add(ColorType.Brightyellow, new Color32[] { new(248, 181, 0, byte.MaxValue), new(255, 102, 0, byte.MaxValue) });
+            LighterColorData.Add(ColorType.Darkaqua, new Color32[] { new(14, 104, 188, byte.MaxValue), new(11, 85, 153, byte.MaxValue) });
+            NoLighterColorData.Add(ColorType.Matcha, new Color32[] { new(52, 99, 23, byte.MaxValue), new(34, 54, 19, byte.MaxValue) });
+            LighterColorData.Add(ColorType.Pitchwhite, new Color32[] { new(255, 255, 255, byte.MaxValue), new(240, 240, 240, byte.MaxValue) });
+            LighterColorData.Add(ColorType.Darksky, new Color32[] { new(64, 128, 192, byte.MaxValue), new(32, 96, 128, byte.MaxValue) });
+            LighterColorData.Add(ColorType.Intenseblue, new Color32[] { new(83, 136, 255, byte.MaxValue), new(76, 122, 230, byte.MaxValue) });
+            NoLighterColorData.Add(ColorType.Blueclosertoblack, new Color32[] { new(0, 0, 50, byte.MaxValue), new(0, 0, 25, byte.MaxValue) });
+            LighterColorData.Add(ColorType.Sunkengreenishblue, new Color32[] { new(128, 156, 166, byte.MaxValue), new(115, 141, 153, byte.MaxValue) });
+            NoLighterColorData.Add(ColorType.Azi, new Color32[] { new(100, 48, 0, byte.MaxValue), new(98, 5, 0, byte.MaxValue) });
+            LighterColorData.Add(ColorType.Pitchred, new Color32[] { new(255, 0, 0, byte.MaxValue), new(220, 20, 60, byte.MaxValue) });
+            LighterColorData.Add(ColorType.Pitchblue, new Color32[] { new(0, 0, 128, byte.MaxValue), new(0, 0, 112, byte.MaxValue) });
+            LighterColorData.Add(ColorType.Pitchgreen, new Color32[] { new(0, 128, 0, byte.MaxValue), new(0, 120, 0, byte.MaxValue) });
+            LighterColorData.Add(ColorType.Pitchyellow, new Color32[] { new(255, 255, 0, byte.MaxValue), new(255, 235, 0, byte.MaxValue) });
+            LighterColorData.Add(ColorType.Backblue, new Color32[] { new(0, 128, 255, byte.MaxValue), new(0, 85, 255, byte.MaxValue) });
+            LighterColorData.Add(ColorType.Mildpurple, new Color32[] { new(109, 83, 131, byte.MaxValue), new(109, 83, 131, byte.MaxValue) });
+            NoLighterColorData.Add(ColorType.Ashishreddishpurplecolor, new Color32[] { new(139, 102, 118, byte.MaxValue), new(139, 102, 118, byte.MaxValue) });
+            LighterColorData.Add(ColorType.Melon, new Color32[] { new(0, 225, 129, byte.MaxValue), new(24, 255, 81, byte.MaxValue) });
+            NoLighterColorData.Add(ColorType.Crasyublue, new Color32[] { new(2, 38, 106, byte.MaxValue), new(64, 0, 111, byte.MaxValue) });
+            LighterColorData.Add(ColorType.Lightgreen, new Color32[] { new(226, 255, 5, byte.MaxValue), new(192, 201, 10, byte.MaxValue) });
 
 
             List<CustomColor> colors = new();
