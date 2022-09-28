f10b5da4f78e2b42fd0ed78f2a2b220dc1c5e45e
ykundesu <4476tv@gmail.com>
2022-09-28 21:41:53 +0900

    要らないものを削除

--- SuperNewRoles/Helpers/Il2CppHelpers.cs
+++ SuperNewRoles/Helpers/Il2CppHelpers.cs
@@ -6,49 +6,6 @@ using UnhollowerRuntimeLib;
 
 namespace SuperNewRoles
 {
-    [AttributeUsage(AttributeTargets.Class)]
-    public sealed class Il2CppRegisterAttribute : Attribute
-    {
-
-        public static void Registration(Assembly dll)
-        {
-            foreach (Type type in dll.GetTypes())
-            {
-                Il2CppRegisterAttribute attribute =
-                    CustomAttributeExtensions.GetCustomAttribute<Il2CppRegisterAttribute>(type);
-                if (attribute != null)
-                {
-                    registrationForTarget(type);
-                }
-            }
-        }
-
-        private static void registrationForTarget(Type targetType)
-        {
-            Type targetBase = targetType.BaseType;
-
-            Il2CppRegisterAttribute baseAttribute =
-                (targetType == null) ?
-                null :
-                CustomAttributeExtensions.GetCustomAttribute<Il2CppRegisterAttribute>(targetBase);
-
-            if (baseAttribute != null)
-            {
-                registrationForTarget(targetType);
-            }
-
-            if (ClassInjector.IsTypeRegisteredInIl2Cpp(targetType)) { return; }
-
-            try
-            {
-                ClassInjector.RegisterTypeInIl2Cpp(targetType);
-            }
-            catch (Exception e)
-            {
-
-            }
-        }
-    }
     public static class Il2CppHelpers
     {
         private static class CastHelper<T> where T : Il2CppObjectBase
