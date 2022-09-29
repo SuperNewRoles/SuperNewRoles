c5c23a2c46009071992a080fb167c662b2f7ce92
ykundesu <4476tv@gmail.com>
2022-09-29 18:00:03 +0900

    ログ削除

--- SuperNewRoles/Roles/Impostor/ConnectKiller.cs
+++ SuperNewRoles/Roles/Impostor/ConnectKiller.cs
@@ -8,11 +8,9 @@ namespace SuperNewRoles.Roles.Impostor
         //ここにコードを書きこんでください
         public static void Update()
         {
-            Logger.Info("アップデート:"+RoleHelpers.IsComms());
             bool CommsData = RoleHelpers.IsComms();
             if (RoleClass.ConnectKiller.OldCommsData != CommsData)
             {
-                Logger.Info("コネクト:"+CommsData);
                 VentDataModules.ConnectAllVent(CommsData);
                 if (Vent.currentVent is not null) Vent.currentVent.SetButtons(true);
                 RoleClass.ConnectKiller.OldCommsData = CommsData;
