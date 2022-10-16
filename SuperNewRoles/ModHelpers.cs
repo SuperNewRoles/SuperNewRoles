10ef28a9a2e3cd66557230eee617840360ae7ef9
Sakurander <sakurander77@gmail.com>
2022-10-16 11:19:55 +0900

    フォーマット

--- SuperNewRoles/ModHelpers.cs
+++ SuperNewRoles/ModHelpers.cs
@@ -771,10 +771,10 @@ namespace SuperNewRoles
         public static bool GetManyKeyDown(KeyCode[] keyCodes) =>
             keyCodes.All(x => Input.GetKey(x)) && keyCodes.Any(x => Input.GetKeyDown(x));
 
-        public static void AddRanges(this List<PlayerControl> list ,List<PlayerControl>[] collections) {
-            foreach(var c in collections) {
+        public static void AddRanges(this List<PlayerControl> list, List<PlayerControl>[] collections)
+        {
+            foreach (var c in collections)
                 list.AddRange(c);
-            }
         }
     }
     public static class CreateFlag
