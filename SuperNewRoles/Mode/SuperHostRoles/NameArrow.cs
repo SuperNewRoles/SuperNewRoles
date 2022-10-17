529438eb1e433834485301c6227dc03c87c0658c
Sakurander <sakurander77@gmail.com>
2022-10-18 01:20:14 +0900

    フォーマット

--- SuperNewRoles/Mode/SuperHostRoles/NameArrow.cs
+++ SuperNewRoles/Mode/SuperHostRoles/NameArrow.cs
@@ -6,7 +6,7 @@ namespace SuperNewRoles.Mode.SuperHostRoles
 {
     public static class NameArrow
     {
-        private static readonly string[]strings = new[] { "↑", "↗", "→", "↘", "↓", "↙", "←", "↖" };
+        private static readonly string[] strings = new[] { "↑", "↗", "→", "↘", "↓", "↙", "←", "↖" };
 
         static float GetAngle(Vector2 start, Vector2 target)
         {
@@ -22,20 +22,21 @@ namespace SuperNewRoles.Mode.SuperHostRoles
             return degree;
         }
 
-        private static bool floatRange(float f1 ,float f,float f2)=>f1<f&&f<f2;
+        private static bool floatRange(float f1, float f, float f2) => f1 < f && f < f2;
 
 
-        public static string arrowAngleString(){
-            var angle =  GetAngle(PlayerControl.LocalPlayer.GetTruePosition(),new Vector2(0,0));
-            if (floatRange(337.5f,angle,360f)||floatRange(0f,angle,22.5f))return strings[0];
-            if (floatRange(22.5f,angle,67.5f)) return strings[1];
-            if (floatRange(67.5f,angle,112.5f)) return strings[2];
-            if (floatRange(112.5f,angle,157.5f)) return strings[3];
-            if (floatRange(157.5f,angle,202.5f)) return strings[4];
-            if (floatRange(202.5f,angle,247.5f)) return strings[5];
-            if (floatRange(247.5f,angle,292.5f)) return strings[6];
-            if (floatRange(292.5f,angle,337.5f)) return strings[7];
-            Logger.Info(angle.ToString(),"namearr");
+        public static string arrowAngleString()
+        {
+            var angle = GetAngle(PlayerControl.LocalPlayer.GetTruePosition(), new Vector2(0, 0));
+            if (floatRange(337.5f, angle, 360f) || floatRange(0f, angle, 22.5f)) return strings[0];
+            if (floatRange(22.5f, angle, 67.5f)) return strings[1];
+            if (floatRange(67.5f, angle, 112.5f)) return strings[2];
+            if (floatRange(112.5f, angle, 157.5f)) return strings[3];
+            if (floatRange(157.5f, angle, 202.5f)) return strings[4];
+            if (floatRange(202.5f, angle, 247.5f)) return strings[5];
+            if (floatRange(247.5f, angle, 292.5f)) return strings[6];
+            if (floatRange(292.5f, angle, 337.5f)) return strings[7];
+            Logger.Info(angle.ToString(), "namearr");
             return "";
         }
     }
