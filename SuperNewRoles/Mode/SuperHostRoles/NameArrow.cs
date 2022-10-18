4243b1a7141e073b88170a20e5880ca87d5ea1c1
Sakurander <sakurander77@gmail.com>
2022-10-18 18:47:01 +0900

    配列からではなく直接文字列を取得するように変更

--- SuperNewRoles/Mode/SuperHostRoles/NameArrow.cs
+++ SuperNewRoles/Mode/SuperHostRoles/NameArrow.cs
@@ -4,8 +4,6 @@ namespace SuperNewRoles.Mode.SuperHostRoles
 {
     public static class NameArrow
     {
-        private static readonly string[] strings = new[] { "↑", "↗", "→", "↘", "↓", "↙", "←", "↖" };
-
         static float GetAngle(Vector2 start, Vector2 target)
         {
             Vector2 dt = target - start;
@@ -26,14 +24,14 @@ namespace SuperNewRoles.Mode.SuperHostRoles
         public static string ArrowAngleString(Vector2 from,Vector2 to)
         {
             var angle = GetAngle(from, to);
-            if (FloatRange(337.5f, angle, 360f) || FloatRange(0f, angle, 22.5f)) return strings[0];
-            if (FloatRange(22.5f, angle, 67.5f)) return strings[1];
-            if (FloatRange(67.5f, angle, 112.5f)) return strings[2];
-            if (FloatRange(112.5f, angle, 157.5f)) return strings[3];
-            if (FloatRange(157.5f, angle, 202.5f)) return strings[4];
-            if (FloatRange(202.5f, angle, 247.5f)) return strings[5];
-            if (FloatRange(247.5f, angle, 292.5f)) return strings[6];
-            if (FloatRange(292.5f, angle, 337.5f)) return strings[7];
+            if (FloatRange(337.5f, angle, 360f) || FloatRange(0f, angle, 22.5f)) return "↑";
+            if (FloatRange(22.5f, angle, 67.5f)) return "↗";
+            if (FloatRange(67.5f, angle, 112.5f)) return "→";
+            if (FloatRange(112.5f, angle, 157.5f)) return "↘";
+            if (FloatRange(157.5f, angle, 202.5f)) return "↓";
+            if (FloatRange(202.5f, angle, 247.5f)) return "↙";
+            if (FloatRange(247.5f, angle, 292.5f)) return "←";
+            if (FloatRange(292.5f, angle, 337.5f)) return "↖";
             Logger.Info(angle.ToString(), "namearr");
             return "";
         }
