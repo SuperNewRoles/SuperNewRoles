6e21a26120bcbccbf7e6522788e4bda8945c75a2
月城蔵徒 <104145991+Kurato-Tsukishiro@users.noreply.github.com>
2022-11-04 01:53:28 +0900

    Crewを切ってシェリフとりもしぇりが自爆するように

--- SuperNewRoles/Modules/CustomRPC.cs
+++ SuperNewRoles/Modules/CustomRPC.cs
@@ -796,6 +796,8 @@ namespace SuperNewRoles.Modules
             if (MissFire)
             {
                 sheriff.MurderPlayer(sheriff);
+                if(sheriff.IsRole(RoleId.Sheriff) && CustomOptionHolder.SheriffCanKillCrewmate.GetBool()) target.MurderPlayer(target);
+                else if(sheriff.IsRole(RoleId.RemoteSheriff) && CustomOptionHolder.RemoteSheriffCanKillCrewmate.GetBool()) target.MurderPlayer(target);
             }
             else
             {
