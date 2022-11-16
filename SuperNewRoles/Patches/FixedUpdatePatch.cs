728c4bfdb9392802eba2ec673ff6bbb8210a794d
月城蔵徒 <104145991+Kurato-Tsukishiro@users.noreply.github.com>
2022-11-16 12:31:39 +0900

    無駄に2つあった廃村のlogを一つにした

--- SuperNewRoles/Patches/FixedUpdatePatch.cs
+++ SuperNewRoles/Patches/FixedUpdatePatch.cs
@@ -49,16 +49,14 @@ namespace SuperNewRoles.Patches
                     RPCProcedure.SetHaison();
                     if (ModeHandler.IsMode(ModeId.SuperHostRoles))
                     {
-                        Logger.Info("===================== 廃村 ======================", "End Game");
                         EndGameCheck.CustomEndGame(ShipStatus.Instance, GameOverReason.ImpostorDisconnect, false);
-
                     }
                     else
                     {
-                        Logger.Info("===================== 廃村 =====================", "End Game");
                         ShipStatus.RpcEndGame(GameOverReason.ImpostorDisconnect, false);
                         MapUtilities.CachedShipStatus.enabled = false;
                     }
+                    Logger.Info("===================== 廃村 ======================", "End Game");
                 }
             }
 
