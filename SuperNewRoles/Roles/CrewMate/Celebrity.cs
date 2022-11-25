76d1ce33af51b3645b123debfa1ca2dc205e3bf0
月城蔵徒 <104145991+Kurato-Tsukishiro@users.noreply.github.com>
2022-11-25 17:20:18 +0900

    logの文章変更()

--- SuperNewRoles/Roles/CrewMate/Celebrity.cs
+++ SuperNewRoles/Roles/CrewMate/Celebrity.cs
@@ -11,25 +11,25 @@ namespace SuperNewRoles.Roles.Crewmate
     {
         private static Timer timer;
 
-        public static void TaskPhaseTimer()
+        public static void CelebrityTimerSet()
         {
             timer = new Timer(RoleClass.Celebrity.FlashTime);
             timer.Elapsed += TimerEvent;
             timer.AutoReset = true;
             timer.Enabled = true;
-            Logger.Info("タイマーセット!");
+            Logger.Info($"{RoleClass.Celebrity.FlashTime}[ミリ秒]にタイマーセット ", "CelebrityFlash");
+
         }
 
         private static void TimerEvent(System.Object source, ElapsedEventArgs e)
         {
             Seer.ShowFlash(Color.yellow);
-            Logger.Info("フラッシュ!!");
+            Logger.Info($"{RoleClass.Celebrity.FlashTime / 1000}s 経過した為発光しました。", "CelebrityFlash");
         }
 
         public static void WrapUp()
         {
-            TaskPhaseTimer();
-            Logger.Info($"{RoleClass.Celebrity.FlashTime}");
+            CelebrityTimerSet();
         }
     }
 }
