cbe9a3c5b403e10574e32d87b4dd71d250b7517b
月城蔵徒 <104145991+Kurato-Tsukishiro@users.noreply.github.com>
2022-11-26 02:13:34 +0900

    TimerEvent形式からラムザ形式のイベント表記を変えた

--- SuperNewRoles/Roles/CrewMate/Celebrity.cs
+++ SuperNewRoles/Roles/CrewMate/Celebrity.cs
@@ -14,16 +14,14 @@ namespace SuperNewRoles.Roles.Crewmate
         public static void CelebrityTimerSet()
         {
             timer = new Timer(RoleClass.Celebrity.FlashTime);
-            timer.Elapsed += TimerEvent;
+            timer.Elapsed += (source, e) =>
+            {
+                Seer.ShowFlash(Color.yellow);
+                Logger.Info($"{RoleClass.Celebrity.FlashTime / 1000}s 経過した為発光しました。", "CelebrityFlash");
+            };
             timer.AutoReset = true;
             timer.Enabled = true;
             Logger.Info($"{RoleClass.Celebrity.FlashTime}[ミリ秒]にタイマーセット ", "CelebrityFlash");
-        }
-
-        private static void TimerEvent(System.Object source, ElapsedEventArgs e)
-        {
-            Seer.ShowFlash(Color.yellow);
-            Logger.Info($"{RoleClass.Celebrity.FlashTime / 1000}s 経過した為発光しました。", "CelebrityFlash");
         }
 
         public static void WrapUp()
