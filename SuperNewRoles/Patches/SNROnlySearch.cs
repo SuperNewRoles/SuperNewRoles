9bb2aaf1e1b0d866517c0e24ae8834032f45bdd4
ykundesu <4476tv@gmail.com>
2022-12-14 19:39:48 +0900

    ボタンの状態に反映されるように

--- SuperNewRoles/Patches/SNROnlySearch.cs
+++ SuperNewRoles/Patches/SNROnlySearch.cs
@@ -11,72 +11,35 @@ namespace SuperNewRoles.Patches;
 
 public static class SNROnlySearch
 {
-    public static byte currentMapId;
-    static KeyValueOption MapNameOption;
-    public static bool IsSNRSearch = false;
-    public static void FixedUpdate()
-    {
-        //Logger.Info(GameOptionsManager.Instance.CurrentGameOptions.MapId.ToString());
-        if (AmongUsClient.Instance.AmHost)
-        {
-            if (CustomOptionHolder.IsSNROnlySearch.GetBool())
-            {
-                if (GameOptionsManager.Instance.CurrentGameOptions.MapId < 5)
-                {
-                    currentMapId = GameOptionsManager.Instance.CurrentGameOptions.MapId;
-                    GameOptionsManager.Instance.CurrentGameOptions.SetByte(ByteOptionNames.MapId, 6);
-                    PlayerControl.LocalPlayer.RpcSyncSettings(GameOptionsManager.Instance.gameOptionsFactory.ToBytes(GameOptionsManager.Instance.CurrentGameOptions));
-                }
-                MessageWriter writer = RPCHelper.StartRPC(CustomRPC.SetMapId);
-                writer.Write(currentMapId);
-                writer.EndRPC();
-                if (GameSettingMenu.Instance != null)
-                {
-                    if (MapNameOption == null)
-                        MapNameOption = GameSettingMenu.Instance.transform.FindChild("Game Settings/GameGroup/SliderInner/MapName")?.GetComponent<KeyValueOption>();
-                    if (MapNameOption != null)
-                    {
-                        MapNameOption.oldValue = currentMapId;
-                        MapNameOption.ValueText.text = Constants.MapNames[currentMapId];
-                    }
-                }
-            }
-            else
-            {
-                if (GameOptionsManager.Instance.CurrentGameOptions.MapId > 5)
-                {
-                    currentMapId = 0;
-                    GameOptionsManager.Instance.CurrentGameOptions.SetByte(ByteOptionNames.MapId, currentMapId);
-                    PlayerControl.LocalPlayer.RpcSyncSettings(GameOptionsManager.Instance.gameOptionsFactory.ToBytes(GameOptionsManager.Instance.CurrentGameOptions));
-                }
-            }
-        }
-    }
-    [HarmonyPatch(typeof(FilterTagsMenu), nameof(FilterTagsMenu.Open))]
-    public static class FilterTagsMenuOpenPatch
-    {
-        public static void Prefix() => DataManager.Settings.Multiplayer.ValidGameFilterOptions.FilterTags.Add("SNR");
-    }
+    public const string FilterText = "SNR";
     [HarmonyPatch(typeof(FilterTagsMenu), nameof(FilterTagsMenu.ChooseOption))]
     public static class FilterTagsMenuChooseOptionPatch
     {
         public static void Postfix(FilterTagsMenu __instance, ChatLanguageButton button, string filter)
         {
-            if (__instance.targetOpts.FilterTags.Contains("SNR"))
+            if (__instance.targetOpts.FilterTags.Contains(FilterText))
             {
-                if (filter == "SNR")
+                if (filter == FilterText)
                 {
                     __instance.targetOpts.FilterTags = new();
-                    __instance.targetOpts.FilterTags.Add("SNR");
-                    //foreach (var a in __instance.controllerSelectable.)
+                    __instance.targetOpts.FilterTags.Add(FilterText);
+                    foreach (var btn in __instance.controllerSelectable)
                     {
-
+                        btn.GetComponent<ChatLanguageButton>().SetSelected(false);
                     }
+                    button.SetSelected(true);
                 }
                 else
                 {
-                    __instance.targetOpts.FilterTags.Remove("SNR");
+                    __instance.targetOpts.FilterTags.Remove(FilterText);
+                    foreach (var btn in __instance.controllerSelectable)
+                    {
+                        ChatLanguageButton LangBtn = btn.GetComponent<ChatLanguageButton>();
+                        if (LangBtn.Text.text == FilterText)
+                            LangBtn.SetSelected(false);
+                    }
                 }
+                __instance.UpdateButtonText();
             }
         }
     }
