9c15fec2b4f87f2af6e7621baad7ae6d68b2ad3a
ykundesu <4476tv@gmail.com>
2022-12-22 22:16:14 +0900

    メリクリテキスト

--- SuperNewRoles/Roles/Neutral/LoversBreaker.cs
+++ SuperNewRoles/Roles/Neutral/LoversBreaker.cs
@@ -29,10 +29,15 @@ namespace SuperNewRoles.Roles.Neutral
                 Popup.gameObject.SetActive(true);
                 Popup.transform.FindChild("Background").localScale = new(2, 2.8f, 1);
                 Popup.transform.FindChild("ExitGame").localPosition = new(0f, -2f, -0.5f);
+                Popup.transform.FindChild("ExitGame").GetComponentInChildren<TextMeshPro>().text = "了解！";
                 TextMeshPro Title = GameObject.Instantiate(Popup.TextAreaTMP, Popup.transform);
-                Popup.TextAreaTMP.transform.localPosition = new(-2, 1.8f, -0.5f);
-                Popup.TextAreaTMP.transform.localScale = Vector3.one * 1.5f;
-                Popup.TextAreaTMP.text = "メリークリスマス！\n皆さんに2つの新役職のプレゼントを用意しました！\n「爆ぜ師」と「ジャンボ」です！\n詳しくはTwitterをチェック！\n\n\n@SNRDevs";
+                Title.text = "メリークリスマス！";
+                Title.transform.localPosition = new(0.15f, 2, -0.5f);
+                Title.transform.localScale = Vector3.one * 4.5f;
+                Popup.TextAreaTMP.transform.localPosition = new(0, 0.5f, -0.5f);
+                Popup.TextAreaTMP.transform.localScale = Vector3.one * 2.3f;
+                Popup.TextAreaTMP.text = "メリークリスマス！\n皆さんに2つの新役職のプレゼントを用意しました！\n「爆ぜ師」と「ジャンボ」です！\n詳しくはTwitterをチェック！\n\n\n<link=\"https://twitter.com/SNRDevs\">@SNRDevs</link>";
+                Popup.destroyOnClose = true;
             }
         }
     }
