c1f88207ac208c9d14e142e0c429322c21e7e8bd
ykundesu <4476tv@gmail.com>
2022-12-22 20:53:14 +0900

    ターゲットのアウトラインを追加

--- SuperNewRoles/Modules/CustomOptionModel.cs
+++ SuperNewRoles/Modules/CustomOptionModel.cs
@@ -808,7 +808,7 @@ static class GameOptionsMenuUpdatePatch
     }
     public static bool IsHidden(this CustomOption option)
     {
-        return option.isHidden || (!option.isSHROn && ModeHandler.IsMode(ModeId.SuperHostRoles, false)) || option == CustomOptionHolder.LoversBreakerOption || option == CustomOptionHolder.JumboOption;// || ((option == CustomOptionHolder.LoversBreakerOption) && DateTime.UtcNow < new DateTime(2022,12,23,12,0,0));
+        return option.isHidden || (!option.isSHROn && ModeHandler.IsMode(ModeId.SuperHostRoles, false)) || option == CustomOptionHolder.JumboOption;// || ((option == CustomOptionHolder.LoversBreakerOption) && DateTime.UtcNow < new DateTime(2022,12,23,12,0,0));
     }
     public static void Postfix(GameOptionsMenu __instance)
     {
