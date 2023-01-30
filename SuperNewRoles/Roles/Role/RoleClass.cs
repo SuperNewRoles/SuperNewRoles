749db0507d9cbf631e79c05c26a6c24aa43c05b2
月城蔵徒 <104145991+Kurato-Tsukishiro@users.noreply.github.com>
2023-01-30 18:27:19 +0900

    デバッカーに出ていたnull問題を修正

--- SuperNewRoles/Roles/Role/RoleClass.cs
+++ SuperNewRoles/Roles/Role/RoleClass.cs
@@ -218,7 +218,7 @@ public static class RoleClass
 
         public static void ClearAndReload()
         {
-            AmDebugger = AmongUsClient.Instance.AmHost && CustomOptionHolder.DebuggerOption.GetBool();
+            AmDebugger = AmongUsClient.Instance.AmHost && ConfigRoles.DebugMode.Value && CustomOptionHolder.DebuggerOption.GetBool();
         }
     }
 
