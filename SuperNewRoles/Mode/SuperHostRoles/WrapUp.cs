1cf0a7b9266776824f668d8d851ae5e5cd11e91d
月城蔵徒 <104145991+Kurato-Tsukishiro@users.noreply.github.com>
2023-02-01 17:53:36 +0900

    Revert "「死亡後インポゴーストにする」処理と「追放された時にインポゴーストにする」処理で重複していた為追放時の処理は消した(SNR)"
    This reverts commit 8f862d0e171e8c227c66306f47e83ea1e49f852c.

--- SuperNewRoles/Mode/SuperHostRoles/WrapUp.cs
+++ SuperNewRoles/Mode/SuperHostRoles/WrapUp.cs
@@ -85,5 +85,6 @@ class WrapUpClass
         }
         Roles.Jester.WrapUp(exiled);
         Roles.Nekomata.WrapUp(exiled);
+        if (exiled.Object.IsShapeshifter()) exiled.Object.ResetAndSetImpostorghost();
     }
 }
