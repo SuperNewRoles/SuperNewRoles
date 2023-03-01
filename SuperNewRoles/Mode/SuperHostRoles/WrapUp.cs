2e129b49bfd4edbd277869c1b94536c06eab0df1
月城蔵徒 <104145991+Kurato-Tsukishiro@users.noreply.github.com>
2023-03-01 10:48:29 +0900

    シェイプシフター系が死亡時Impostorghostに変更するコード削除

--- SuperNewRoles/Mode/SuperHostRoles/WrapUp.cs
+++ SuperNewRoles/Mode/SuperHostRoles/WrapUp.cs
@@ -89,6 +89,5 @@ class WrapUpClass
         }
         Roles.Jester.WrapUp(exiled);
         Roles.Nekomata.WrapUp(exiled);
-        if (exiled.Object.IsShapeshifter()) exiled.Object.ResetAndSetImpostorghost();
     }
 }
--- SuperNewRoles/Roles/Role/RoleHelper.cs
+++ SuperNewRoles/Roles/Role/RoleHelper.cs
@@ -43,25 +43,6 @@ public static class RoleHelpers
     {
         return !player.IsRole(RoleId.Sheriff, RoleId.Sheriff) && player != null && player.Data.Role.IsImpostor;
     }
-
-    /// <summary>
-    /// v2022.12.8で発生したシェイプシフターが死亡後クルーメイトゴーストになるバグの修正用。
-    /// We are Shapeshifter!
-    /// </summary>
-    /// <param name="player">シェイプシフターであるか判定されるプレイヤー</param>
-    /// <returns>プレイヤーがシェイプシフターである場合trueを返す</returns>
-    public static bool IsShapeshifter(this PlayerControl player) =>
-        player.GetRole() is
-        RoleId.SelfBomber or
-        RoleId.Samurai or
-        RoleId.EvilButtoner or
-        RoleId.SuicideWisher or
-        RoleId.Doppelganger or
-        RoleId.Camouflager or
-        RoleId.EvilSeer or
-        RoleId.ShiftActor;
-    // IsShapeshifter
-
     public static bool IsHauntedWolf(this PlayerControl player) => player.IsRole(RoleId.HauntedWolf);
 
     /// <summary>
@@ -1426,7 +1407,6 @@ public static class RoleHelpers
     {
         var IsTaskClear = false;
         if (player.IsImpostor()) IsTaskClear = true;
-        if (player.IsShapeshifter()) IsTaskClear = true;
         if (player.IsMadRoles()) IsTaskClear = true;
         if (player.IsFriendRoles()) IsTaskClear = true;
         if (player.IsNeutral()) IsTaskClear = true;
