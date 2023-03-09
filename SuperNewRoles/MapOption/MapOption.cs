4833aa04ae1dda6503bb65087ed96bd910165986
月城蔵徒 <104145991+Kurato-Tsukishiro@users.noreply.github.com>
2023-03-09 12:25:14 +0900

    Revert "書き方が変だったので修正"
    This reverts commit f9300d3481a32fee2b158999e68b3dfb0d1957b5.

--- SuperNewRoles/MapOption/MapOption.cs
+++ SuperNewRoles/MapOption/MapOption.cs
@@ -57,7 +57,7 @@ public class MapOption
 
     // |:========== その他 ==========:|
     public static Dictionary<byte, PoolablePlayer> playerIcons = new();
-    private static readonly string color = "<color=#ffeefa>";
+    private readonly static string color = "<color=#ffeefa>";
 
 
     public static void LoadOption()
