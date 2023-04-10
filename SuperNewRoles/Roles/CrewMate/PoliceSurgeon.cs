8cdd8a8724647e6028c04b4352e9837a84822ea8
月城蔵徒 <104145991+Kurato-Tsukishiro@users.noreply.github.com>
2023-04-10 09:07:27 +0900

    仕様に書いた設定追加

--- SuperNewRoles/Roles/CrewMate/PoliceSurgeon.cs
+++ SuperNewRoles/Roles/CrewMate/PoliceSurgeon.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using static SuperNewRoles.Modules.CustomOption;
 
 namespace SuperNewRoles.Roles.Crewmate;
 
@@ -8,10 +9,25 @@ public static class PoliceSurgeon
     private static int optionId = 1266;
     public static CustomRoleOption PoliceSurgeonOption;
     public static CustomOption PoliceSurgeonPlayerCount;
+    public static CustomOption PoliceSurgeonHaveVitalsInTaskPhase;
+    public static CustomOption PoliceSurgeonVitalsDisplayCooldown;
+    public static CustomOption PoliceSurgeonBatteryDuration;
+    public static CustomOption PoliceSurgeonIndicateTimeOfDeathInSubsequentTurn;
+    public static CustomOption PoliceSurgeonHowManyTurnAgoTheDied;
+    public static CustomOption PoliceSurgeonIncludeAnErrorInTheTimeOfDeath;
+    public static CustomOption PoliceSurgeonMarginOfErrorToIncludeInTimeOfDeath;
+
     public static void SetupCustomOptions()
     {
-        PoliceSurgeonOption = CustomOption.SetupCustomRoleOption(optionId, true, RoleId.PoliceSurgeon); optionId++;
-        PoliceSurgeonPlayerCount = CustomOption.Create(optionId, true, CustomOptionType.Crewmate, "SettingPlayerCountName", CustomOptionHolder.CrewPlayers[0], CustomOptionHolder.CrewPlayers[1], CustomOptionHolder.CrewPlayers[2], CustomOptionHolder.CrewPlayers[3], PoliceSurgeonOption); optionId++;
+        PoliceSurgeonOption = SetupCustomRoleOption(optionId, true, RoleId.PoliceSurgeon); optionId++;
+        PoliceSurgeonPlayerCount = Create(optionId, true, CustomOptionType.Crewmate, "SettingPlayerCountName", CustomOptionHolder.CrewPlayers[0], CustomOptionHolder.CrewPlayers[1], CustomOptionHolder.CrewPlayers[2], CustomOptionHolder.CrewPlayers[3], PoliceSurgeonOption); optionId++;
+        PoliceSurgeonHaveVitalsInTaskPhase = Create(optionId, true, CustomOptionType.Crewmate, "PoliceSurgeonHaveVitalsInTaskPhase", false, PoliceSurgeonOption); optionId++;
+        PoliceSurgeonVitalsDisplayCooldown = Create(optionId, true, CustomOptionType.Crewmate, "VitalsDisplayCooldown", 15f, 5f, 60f, 5f, PoliceSurgeonHaveVitalsInTaskPhase); optionId++;
+        PoliceSurgeonBatteryDuration = Create(optionId, true, CustomOptionType.Crewmate, "BatteryDuration", 5f, 5f, 30f, 5f, PoliceSurgeonHaveVitalsInTaskPhase); optionId++;
+        PoliceSurgeonIndicateTimeOfDeathInSubsequentTurn = Create(optionId, true, CustomOptionType.Crewmate, "PoliceSurgeonIndicateTimeOfDeathInSubsequentTurn", true, PoliceSurgeonOption); optionId++;
+        PoliceSurgeonHowManyTurnAgoTheDied = Create(optionId, true, CustomOptionType.Crewmate, "PoliceSurgeonHowManyTurnAgoTheDied", false, PoliceSurgeonOption); optionId++;
+        PoliceSurgeonIncludeAnErrorInTheTimeOfDeath = Create(optionId, true, CustomOptionType.Crewmate, "PoliceSurgeonIncludeAnErrorInTheTimeOfDeath", true, PoliceSurgeonOption); optionId++;
+        PoliceSurgeonMarginOfErrorToIncludeInTimeOfDeath = Create(optionId, true, CustomOptionType.Crewmate, "PoliceSurgeonMarginOfErrorToIncludeInTimeOfDeath", 5f, 1f, 15f, 1f,  PoliceSurgeonIncludeAnErrorInTheTimeOfDeath);
     }
 
     public static List<PlayerControl> PoliceSurgeonPlayer;
