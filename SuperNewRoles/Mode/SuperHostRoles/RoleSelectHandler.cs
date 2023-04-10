7840ed7a519dde2b543b1dcdfe3e0fb77c14e1a6
月城蔵徒 <104145991+Kurato-Tsukishiro@users.noreply.github.com>
2023-04-10 18:04:26 +0900

    SHR時導入者(ホストゲスト両方)で科学者になるようにした。

--- SuperNewRoles/Mode/SuperHostRoles/RoleSelectHandler.cs
+++ SuperNewRoles/Mode/SuperHostRoles/RoleSelectHandler.cs
@@ -129,7 +129,7 @@ public static class RoleSelectHandler
         /*============エンジニアに役職設定============*/
 
         /*============科学者に役職設定============*/
-        if (PoliceSurgeon.PoliceSurgeonHaveVitalsInTaskPhase.GetBool()) SetVanillaRole(PoliceSurgeon.PoliceSurgeonPlayer, RoleTypes.Scientist);
+        if (PoliceSurgeon.PoliceSurgeonHaveVitalsInTaskPhase.GetBool()) SetVanillaRole(PoliceSurgeon.PoliceSurgeonPlayer, RoleTypes.Scientist, false);
         /*============科学者に役職設定============*/
 
         /*============シェイプシフターDesync============*/
