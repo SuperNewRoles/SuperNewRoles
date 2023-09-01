edc95090406b8af19ceff8c75c0b9ec6ba233127
ykundesu <49261069+ykundesu@users.noreply.github.com>
2023-09-01 22:46:09 +0900

    リプレイと化け狸修正

--- SuperNewRoles/Patches/EndGamePatch.cs
+++ SuperNewRoles/Patches/EndGamePatch.cs
@@ -487,7 +487,7 @@ public static class OnGameEndPatch
             }
             catch { }
         }
-        if (ConfigRoles.IsSendAnalytics.Value && !SuperNewRolesPlugin.IsBeta && !ConfigRoles.DebugMode.Value)
+       if (!ReplayManager.IsReplayMode && ConfigRoles.IsSendAnalytics.Value && !SuperNewRolesPlugin.IsBeta && !ConfigRoles.DebugMode.Value)
         {
             try
             {
@@ -569,6 +569,21 @@ public static class OnGameEndPatch
                 });
             }
         }
+        if (ReplayManager.IsReplayMode) 
+         { 
+             Logger.Info("ComeEndReplay"); 
+             var ReplayEndGameData = ReplayLoader.ReplayTurns[ReplayLoader.CurrentTurn].CurrentEndGameData; 
+             if (ReplayEndGameData == null) return; 
+             Logger.Info("EndNullReplay"); 
+             Il2CppSystem.Collections.Generic.List<WinningPlayerData> WinningPlayers = new(); 
+             foreach (byte winnerid in ReplayEndGameData.WinnerPlayers) 
+             { 
+                 WinningPlayers.Add(new(GameData.Instance.GetPlayerById(winnerid))); 
+             } 
+             TempData.winners = WinningPlayers; 
+             AdditionalTempData.winCondition = ReplayEndGameData.WinCond; 
+             return; 
+         }
         // Remove Jester, Arsonist, Vulture, Jackal, former Jackals and Sidekick from winners (if they win, they'll be readded)
         List<PlayerControl> notWinners = new();
         List<PlayerControl> peculiarNotWinners = new();
@@ -651,7 +666,7 @@ public static class OnGameEndPatch
             WaveCannonJackal.WaveCannonJackalPlayer,
             WaveCannonJackal.SidekickWaveCannonPlayer,
             BlackHatHacker.BlackHatHackerPlayer,
-            Moira.MoiraPlayer,
+            Moira.MoiraPlayer,                                 Roles.Impostor.MadRole.MadRaccoon.RoleData.Player
             });
         notWinners.AddRange(RoleClass.Cupid.CupidPlayer);
         notWinners.AddRange(RoleClass.Dependents.DependentsPlayer);
