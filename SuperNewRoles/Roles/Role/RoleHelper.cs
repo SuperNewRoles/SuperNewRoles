3afa92d7166a70fd27dc818437ec773e86096bf1
ykundesu <4476tv@gmail.com>
2023-09-01 21:32:47 +0900

    GetRole修正

--- SuperNewRoles/Roles/Role/RoleHelper.cs
+++ SuperNewRoles/Roles/Role/RoleHelper.cs
@@ -1785,7 +1785,7 @@ public static class RoleHelpers
     {
         if (IsChache)
         {
-            return ChacheManager.MyRoleChache.TryGetValue(player.PlayerId, out RoleId roleId) ? roleId : RoleId.DefaultRole;
+            return CacheManager.MyRoleChache != null && ChacheManager.MyRoleChache.TryGetValue(player.PlayerId, out RoleId roleId) ? roleId : RoleId.DefaultRole;
         }
         try
         {
