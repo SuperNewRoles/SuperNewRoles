1533cc1a0302aa786ccf4ef8243ef99b13f5bca1
Kurato-Tsukishiro <104145991+Kurato-Tsukishiro@users.noreply.github.com>
2023-09-19 14:17:24 +0900

    設定作成

--- SuperNewRoles/Roles/Neutral/Crook.cs
+++ SuperNewRoles/Roles/Neutral/Crook.cs
@@ -10,11 +10,15 @@ public static class Crook
         private const int optionId = 303600;
         public static CustomRoleOption Option;
         public static CustomOption PlayerCount;
+        public static CustomOption NumberOfInsuranceClaimsReceivedRequiredToWin;
+        public static CustomOption TimeTheAbilityToInsureOthersIsAvailable;
 
         public static void SetupCustomOptions()
         {
             Option = CustomOption.SetupCustomRoleOption(optionId, true, RoleId.Crook);
             PlayerCount = CustomOption.Create(optionId + 1, true, CustomOptionType.Neutral, "SettingPlayerCountName", CustomOptionHolder.CrewPlayers[0], CustomOptionHolder.CrewPlayers[1], CustomOptionHolder.CrewPlayers[2], CustomOptionHolder.CrewPlayers[3], Option);
+            NumberOfInsuranceClaimsReceivedRequiredToWin = CustomOption.Create(optionId + 2, true, CustomOptionType.Neutral, "CrookNumberOfInsuranceClaimsReceivedRequiredToWin", 3f, 1f, 10f, 1f, Option);
+            TimeTheAbilityToInsureOthersIsAvailable = CustomOption.Create(optionId + 3, true, CustomOptionType.Neutral, "CrookTimeTheAbilityToInsureOthersIsAvailable", 10f, 5f, 60f, 5f, Option);
         }
     }
 
@@ -22,10 +26,12 @@ public static class Crook
     {
         public static List<PlayerControl> Player;
         public static Color32 color = new(96, 161, 189, byte.MaxValue);
+        internal static float TimeForAbilityUse { get; private set; }
 
         public static void ClearAndReload()
         {
             Player = new();
+            TimeForAbilityUse = CustomOptionData.TimeTheAbilityToInsureOthersIsAvailable.GetFloat();
         }
     }
 
