3f252050049508c8f23bda7222ec540a05a9e37e
ykundesu <4476tv@gmail.com>
2023-10-25 20:54:38 +0900

    サボ勝利とバージョン更新とコミュ

--- SuperNewRoles/Patches/EndGamePatch.cs
+++ SuperNewRoles/Patches/EndGamePatch.cs
@@ -1598,7 +1598,7 @@ public static class CheckGameEndPatch
                 return true;
             }
         }
-        ISystemType systemType2 = __instance.Systems.ContainsKey(SystemTypes.Reactor) ? __instance.Systems[SystemTypes.Reactor] : null;
+        ISystemType systemType2 = __instance.Systems.ContainsKey(SystemTypes.HeliSabotage) ? __instance.Systems[SystemTypes.HeliSabotage] : null;
         if (systemType2 == null)
         {
             systemType2 = __instance.Systems.ContainsKey(SystemTypes.Laboratory) ? __instance.Systems[SystemTypes.Laboratory] : null;
