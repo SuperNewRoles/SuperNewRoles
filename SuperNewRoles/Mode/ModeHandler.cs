0d4954c4f095b850ba39fa6c2b9e4d648f488e15
Kurato-Tsukishiro <104145991+Kurato-Tsukishiro@users.noreply.github.com>
2024-03-07 22:32:38 +0900

    通常モード以外封印 (人狼は使えるけど, 使われないから分かりやすく全封印)

--- SuperNewRoles/Mode/ModeHandler.cs
+++ SuperNewRoles/Mode/ModeHandler.cs
@@ -136,8 +136,9 @@ public static class ModeHandler
     public static void OptionLoad()
     {
         Mode = new CustomOptionBlank(null);
-        ModeSetting = CustomOption.Create(101200, true, CustomOptionType.Generic, Cs(new Color(252f / 187f, 200f / 255f, 0, 1f), "ModeSetting"), false, Mode, isHeader: true);
-        ThisModeSetting = CustomOption.Create(101300, true, CustomOptionType.Generic, "SettingMode", modes, ModeSetting);
+        ModeSetting = CustomOption.Create(101200, true, CustomOptionType.Generic, Cs(new Color(252f / 187f, 200f / 255f, 0, 1f), "ModeSetting"), false, Mode, isHeader: true, isHidden: true);
+        ModeSetting.selection = 0;
+        ThisModeSetting = CustomOption.Create(101300, true, CustomOptionType.Generic, "SettingMode", modes, ModeSetting, isHidden: true);
         BattleRoyal.BROption.Load();
         Zombie.ZombieOptions.Load();
         RandomColor.RandomColorOptions.Load();
