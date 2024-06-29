0dc237a490ba8ba098a4f68165602083380333ab
Glaceon-471 <glaceonsnr11@gmail.com>
2024-06-29 20:14:23 +0900

    15フレに1回実行するように

--- SuperNewRoles/Patches/LobbyBehaviourPatch.cs
+++ SuperNewRoles/Patches/LobbyBehaviourPatch.cs
@@ -5,19 +5,25 @@ namespace SuperNewRoles.Patches;
 [HarmonyPatch(typeof(LobbyBehaviour))]
 public class LobbyBehaviourPatch
 {
-    [HarmonyPatch(nameof(LobbyBehaviour.Update)), HarmonyPostfix]
+    public static int Frame = 0;
+
+    [HarmonyPatch(nameof(LobbyBehaviour.FixedUpdate)), HarmonyPostfix]
     public static void UpdatePostfix(LobbyBehaviour __instance)
     {
-        ISoundPlayer MapThemeSound = SoundManager.Instance.soundPlayers.Find(x => x.Name.Equals("MapTheme"));
-        if (ConfigRoles.IsMuteLobbyBGM.Value)
+        if (--Frame <= 0)
         {
-            if (MapThemeSound == null) return;
-            SoundManager.Instance.StopNamedSound("MapTheme");
-        }
-        else
-        {
-            if (MapThemeSound != null) return;
-            SoundManager.Instance.CrossFadeSound("MapTheme", __instance.MapTheme, 0.5f);
+            ISoundPlayer MapThemeSound = SoundManager.Instance.soundPlayers.Find(x => x.Name.Equals("MapTheme"));
+            if (ConfigRoles.IsMuteLobbyBGM.Value)
+            {
+                if (MapThemeSound == null) return;
+                SoundManager.Instance.StopNamedSound("MapTheme");
+            }
+            else
+            {
+                if (MapThemeSound != null) return;
+                SoundManager.Instance.CrossFadeSound("MapTheme", __instance.MapTheme, 0.5f);
+            }
+            Frame = 15;
         }
     }
 }
