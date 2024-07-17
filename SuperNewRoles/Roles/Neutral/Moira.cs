077929ecc17553f9bb02079a8c1f085c075ddc0a
Glaceon-471 <glaceonsnr11@gmail.com>
2024-07-18 02:00:42 +0900

    たぶん選択した人が何らかの要因で死んだ場合入れ替えない様に

--- SuperNewRoles/Roles/Neutral/Moira.cs
+++ SuperNewRoles/Roles/Neutral/Moira.cs
@@ -11,6 +11,7 @@ using SuperNewRoles.Roles.Role;
 using SuperNewRoles.Roles.RoleBases;
 using SuperNewRoles.Roles.RoleBases.Interfaces;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 using Object = UnityEngine.Object;
 
 namespace SuperNewRoles.Roles.Neutral;
@@ -96,11 +97,16 @@ public class Moira : RoleBase, INeutral, IMeetingHandler, IWrapUpHandler, INameH
     {
         if (AbilityUsedThisMeeting) return;
         if (IsLimitOver) return;
-        Limit--;
         MessageWriter writer = RpcWriter;
-        writer.Write(Limit);
+        writer.Write((byte)RpcType.SetLimit);
+        writer.Write(--Limit);
+        SendRpc(writer);
+
+        writer = RpcWriter;
+        writer.Write((byte)RpcType.AddChangeData);
         writer.Write(target1.PlayerId);
         writer.Write(target2.PlayerId);
+        writer.Write(true);
         SendRpc(writer);
         AbilityUsedThisMeeting = true;
     }
@@ -377,12 +383,24 @@ public class Moira : RoleBase, INeutral, IMeetingHandler, IWrapUpHandler, INameH
         SetNamesClass.SetPlayerNameText(Player, $"{Player.NameText().text} {"(⇔)".Color(Roleinfo.RoleColor)}");
     }
 
+    public enum RpcType : byte
+    {
+        SetLimit,
+        AddChangeData
+    }
     public void RpcReader(MessageReader reader)
     {
-        Limit = reader.ReadInt32();
-        SwapVoteData = (reader.ReadByte(), reader.ReadByte());
-        ChangeData.Add(SwapVoteData);
-        AbilityUsedThisMeeting = true;
+        switch ((RpcType)reader.ReadByte())
+        {
+            case RpcType.SetLimit:
+                Limit = reader.ReadInt32();
+                break;
+            case RpcType.AddChangeData:
+                SwapVoteData = (reader.ReadByte(), reader.ReadByte());
+                ChangeData.Add(SwapVoteData);
+                AbilityUsedThisMeeting = true;
+                break;
+        }
     }
 
     public bool CanWin(GameOverReason gameOverReason, WinCondition winCondition) => Player.IsAlive() && IsLimitOver;
@@ -441,8 +459,15 @@ public class Moira : RoleBase, INeutral, IMeetingHandler, IWrapUpHandler, INameH
 
     public void EndAntiBlackout()
     {
+        if (SwapVoteData.Item1.GetPlayerControl().IsDead() || SwapVoteData.Item2.GetPlayerControl().IsDead())
+        {
+            MessageWriter writer = RpcWriter;
+            writer.Write((byte)RpcType.SetLimit);
+            writer.Write(++Limit);
+            SendRpc(writer);
+            return;
+        }
         SwapRole(SwapVoteData.Item1, SwapVoteData.Item2);
-        SwapVoteData = new(byte.MaxValue, byte.MaxValue);
     }
 
     public enum Mode
