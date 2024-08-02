0f8f63cf02f3b863bd78a453fd9219287ba76d44
ykundesu <4476tv@gmail.com>
2024-08-02 20:13:30 +0900

    GetClientのキャッシュ、SyncSettingsのちょっとリファクタリング、ToIl2cppListのリファクタリング

--- SuperNewRoles/Roles/Role/RoleClass.cs
+++ SuperNewRoles/Roles/Role/RoleClass.cs
@@ -46,6 +46,7 @@ public static class RoleClass
         ModHelpers.ColorControlDic = new();
         ModHelpers.VentIdControlDic = new();
         ReplayManager.ClearAndReloads();
+        ModHelpers.DestoryClientCache();
         BlockPlayers = new();
         IsMeeting = false;
         IsFirstMeetingEnd = false;
