f8bc4b40bb9806406c6b5f5b3842b3b9e063e69a
ykundesu <49261069+ykundesu@users.noreply.github.com>
2025-01-26 00:27:05 +0900

    feat: 新しい翻訳システムの実装

---
+++ SuperNewRoles/ModTranslation.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Reflection;
+using System.IO;
+
+namespace SuperNewRoles;
+
+public static partial class ModTranslation
+{
+    private static Dictionary<string, string> CurrentTranslations = new();
+    private static Dictionary<SupportedLangs, Dictionary<string, string>> AllTranslations = new();
+    public static void Load()
+    {
+        var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SuperNewRoles.Resources.TranslationData.csv");
+        var reader = new StreamReader(stream);
+
+        Dictionary<string, string> japaneseTranslation = new();
+        Dictionary<string, string> englishTranslation = new();
+
+        while (!reader.EndOfStream)
+        {
+            string line = reader.ReadLine();
+            // 空白かコメントはスキップ
+            if (string.IsNullOrWhiteSpace(line) || line.StartsWith("#"))
+                continue;
+            string[] parts = line.Split(',');
+            if (parts.Length >= 3)
+            {
+                string key = parts[0].Trim();
+                string japanese = parts[1].Trim();
+                string english = parts[2].Trim();
+
+                japaneseTranslation.Add(key, japanese);
+                englishTranslation.Add(key, english);
+            }
+        }
+        AllTranslations[SupportedLangs.Japanese] = japaneseTranslation;
+        AllTranslations[SupportedLangs.English] = englishTranslation;
+        switch (DestroyableSingleton<TranslationController>.Instance.currentLanguage.languageID)
+        {
+            case SupportedLangs.Japanese:
+                CurrentTranslations = AllTranslations[SupportedLangs.Japanese];
+                break;
+            case SupportedLangs.English:
+                CurrentTranslations = AllTranslations[SupportedLangs.English];
+                break;
+            default:
+                CurrentTranslations = AllTranslations[SupportedLangs.Japanese];
+                break;
+        }
+    }
+    public static string GetString(string key)
+    {
+        return CurrentTranslations.TryGetValue(key, out var value) ? value : key;
+    }
+}
