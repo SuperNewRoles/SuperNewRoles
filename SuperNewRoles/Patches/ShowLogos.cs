99ccfa822eb171308635fcf15151974251e17372
ykundesu <49261069+ykundesu@users.noreply.github.com>
2025-01-27 18:38:52 +0900

    修正

--- SuperNewRoles/Patches/ShowLogos.cs
+++ SuperNewRoles/Patches/ShowLogos.cs
@@ -39,14 +39,11 @@ public static class MainMenuLogos
         }
 
         // メインメニュー用ロゴオブジェクトの作成
-        var logoObject = new GameObject("MainMenuLogo")
-        {
-            transform =
-            {
-                localPosition = new Vector3(2.05f, 0.5f, 0),  // 右側に配置
-                localScale = Vector3.one * 0.75f  // 適切なサイズに縮小
-            }
-        };
+        var logoObject = new GameObject("MainMenuLogo");
+        logoObject.transform.SetPositionAndScale(
+            new(2.05f, 0.5f, 0),  // 右側に配置
+            Vector3.one * 0.75f  // 適切なサイズに縮小
+        );
 
         logoObject.AddComponent<SpriteRenderer>().sprite = logo;
     }
