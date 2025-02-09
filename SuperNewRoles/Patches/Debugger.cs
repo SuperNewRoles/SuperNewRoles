950ade8c765e68033e4d2af5d52c981fda61bc20
ykundesu <49261069+ykundesu@users.noreply.github.com>
2025-02-09 16:46:16 +0900

    右側のプレビューをデバッグ中

--- SuperNewRoles/Patches/Debugger.cs
+++ SuperNewRoles/Patches/Debugger.cs
@@ -16,11 +16,31 @@ public static class Debugger
         if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.D))
         {
             Logger.Info("Debugger Clicked");
-            Logger.Info($"CustomOptionManager.TestInt Before: {CustomOptionManager.TestInt}");
-            CustomOptionManager.GetCustomOptions().FirstOrDefault().UpdateSelection(1);
-            Logger.Info($"CustomOptionManager.TestInt After: {CustomOptionManager.TestInt}");
             CustomOptionsMenu.ShowOptionsMenu();
             RoleOptionMenu.ShowRoleOptionMenu(RoleOptionMenuType.Crewmate);
+            var select_check = AssetManager.GetAsset<GameObject>("Option_Check");
+            GameObject obj = new("jtphsehejtrj");
+            Scroller Scroller = new GameObject("SettingsScroller").AddComponent<Scroller>();
+            Scroller.transform.SetParent(RoleOptionMenu.RoleOptionMenuObjectData.MenuObject.transform);
+            Scroller.gameObject.layer = 5;
+
+            Scroller.transform.localScale = Vector3.one;
+            Scroller.transform.localPosition = new Vector3(18f, 0f, 0f);
+            Scroller.allowX = false;
+            Scroller.allowY = true;
+            Scroller.active = true;
+            Scroller.velocity = new Vector2(0, 0);
+            Scroller.ScrollbarYBounds = new FloatRange(0, 0);
+            Scroller.ContentXBounds = new FloatRange(0, 0);
+            Scroller.enabled = true;
+
+            Scroller.Inner = obj.transform;
+            obj.transform.SetParent(Scroller.transform);
+            obj.transform.localScale = Vector3.one;
+            obj.transform.localPosition = Vector3.zero;
+            var select_check_instance = UnityEngine.Object.Instantiate(select_check, obj.transform);
+            select_check_instance.transform.localPosition = new Vector3(17.74f, 0f, 0f);
+            select_check_instance.transform.localScale = Vector3.one * 2;
         }
     }
 }
