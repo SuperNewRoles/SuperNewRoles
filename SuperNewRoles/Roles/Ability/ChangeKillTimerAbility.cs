0fb0089c11f2af3b35c88809744823054058d44d
ykundesu <49261069+ykundesu@users.noreply.github.com>
2025-03-04 02:02:15 +0900

    feat: キルタイマーを変えるAbility、ChangeKillTimerAbilityを追加

---
+++ SuperNewRoles/Roles/Ability/ChangeKillTimerAbility.cs
@@ -0,0 +1,53 @@
+using System;
+using SuperNewRoles.Events;
+using SuperNewRoles.Events.PCEvents;
+using SuperNewRoles.Modules;
+using SuperNewRoles.Modules.Events.Bases;
+
+namespace SuperNewRoles.Roles.Ability;
+
+public class ChangeKillTimerAbility : AbilityBase
+{
+    public Func<float> KillTimerGetter { get; }
+    private EventListener<MurderEventData> MurderEventListener;
+    private EventListener<WrapUpEventData> WrapUpEventListener;
+
+    public ChangeKillTimerAbility(Func<float> killTimerGetter)
+    {
+        KillTimerGetter = killTimerGetter;
+    }
+
+    public override void AttachToLocalPlayer()
+    {
+        MurderEventListener = MurderEvent.Instance.AddListener(OnMurder);
+        WrapUpEventListener = WrapUpEvent.Instance.AddListener(OnWrapUp);
+    }
+
+    public override void DetachToLocalPlayer()
+    {
+        base.DetachToLocalPlayer();
+        MurderEvent.Instance.RemoveListener(MurderEventListener);
+    }
+
+    private void OnMurder(MurderEventData data)
+    {
+        if (Player == null) return;
+        if (data.killer == null) return;
+        if (Player.PlayerId == data.killer.PlayerId)
+        {
+            // キルが発生した時にKillTimerを設定
+            float killTime = KillTimerGetter();
+            ((ExPlayerControl)Player).SetKillTimerUnchecked(killTime, killTime);
+        }
+    }
+
+    private void OnWrapUp(WrapUpEventData data)
+    {
+        if (Player == null) return;
+        new LateTask(() =>
+        {
+            float killTime = KillTimerGetter();
+            ((ExPlayerControl)Player).SetKillTimerUnchecked(killTime, killTime);
+        }, 0.5f);
+    }
+}
