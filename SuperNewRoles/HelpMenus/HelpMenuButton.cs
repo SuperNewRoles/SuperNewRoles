83fb983bb1cfdd05c36594e0fab5fd68803e4e94
ykundesu <49261069+ykundesu@users.noreply.github.com>
2025-05-15 00:45:48 +0900

    feat: ヘルプボタンを仮追加

--- SuperNewRoles/HelpMenus/HelpMenuButton.cs
+++ SuperNewRoles/HelpMenus/HelpMenuButton.cs
@@ -1,5 +1,8 @@
+
 using HarmonyLib;
 using SuperNewRoles.Modules;
+using UnityEngine;
+using UnityEngine.Events;
 
 namespace SuperNewRoles.HelpMenus;
 
@@ -8,6 +11,21 @@ public static class HelpMenusHUuManagerStartPatch
 {
     public static void Postfix(HudManager __instance)
     {
-        AssetManager.Instantiate("HelpButton", __instance.transform);
+        var obj = AssetManager.Instantiate("HelpButton", __instance.transform);
+        obj.transform.localScale = Vector3.one * 0.072f;
+        // ヘルプメニュー自体と被らないように-499
+        var aspectPosition = obj.AddComponent<AspectPosition>();
+        aspectPosition.Alignment = AspectPosition.EdgeAlignments.RightTop;
+        aspectPosition.DistanceFromEdge = new(2.68f, 0.49f, -499f);
+        aspectPosition.OnEnable();
+        PassiveButton passiveButton = obj.AddComponent<PassiveButton>();
+        passiveButton.Colliders = new Collider2D[] { obj.GetComponent<Collider2D>() };
+        passiveButton.OnClick = new();
+        passiveButton.OnMouseOut = new();
+        passiveButton.OnMouseOver = new();
+        passiveButton.OnClick.AddListener((UnityAction)(() =>
+        {
+            HelpMenuObjectManager.ShowOrHideHelpMenu();
+        }));
     }
 }
