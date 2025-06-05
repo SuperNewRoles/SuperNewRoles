08e3d3cb2195424d545cc42af37883f0f8178acb
Kurato-Tsukishiro <kurato.snr7@gmail.com>
2025-06-05 15:59:53 +0900

    feat: EvilSeerに死体矢印の能力を追加

--- SuperNewRoles/Roles/Impostor/EvilSeer.cs
+++ SuperNewRoles/Roles/Impostor/EvilSeer.cs
@@ -15,7 +15,15 @@ class EvilSeer : RoleBase<EvilSeer>
 {
     public override RoleId Role { get; } = RoleId.EvilSeer;
     public override Color32 RoleColor { get; } = Palette.ImpostorRed;
-    public override List<Func<AbilityBase>> Abilities { get; } = [() => new SeerAbility(new SeerData() { Mode = EvilSeerSeerMode, LimitSoulDuration = EvilSeerLimitSoulDuration, SoulDuration = EvilSeerSoulDuration, ColorMode = EvilSeerColorMode })];
+    public override List<Func<AbilityBase>> Abilities { get; } = [
+        () => new SeerAbility(new SeerData() {
+            Mode = EvilSeerSeerMode,
+            LimitSoulDuration = EvilSeerLimitSoulDuration,
+            SoulDuration = EvilSeerSoulDuration,
+            ColorMode = EvilSeerColorMode
+        }),
+        () => new DeadBodyArrowsAbility(() => EvilSeerShowArrows, colorMode : EvilSeerColorMode),
+    ];
     public override QuoteMod QuoteMod { get; } = QuoteMod.SuperNewRoles;
     public override RoleTypes IntroSoundType { get; } = RoleTypes.Phantom;
     public override short IntroNum { get; } = 1;
@@ -26,8 +34,14 @@ class EvilSeer : RoleBase<EvilSeer>
     public override RoleTag[] RoleTags { get; } = [RoleTag.Information];
     public override RoleOptionMenuType OptionTeam { get; } = RoleOptionMenuType.Impostor;
 
+
+    [CustomOptionSelect("EvilSeer.SeerColorMode", typeof(CrewMate.DeadBodyColorMode), "Seer.SeerColorMode.", translationName: "Seer.SeerColorMode")]
+    public static CrewMate.DeadBodyColorMode EvilSeerColorMode = CrewMate.DeadBodyColorMode.None;
+
+
     [CustomOptionSelect("EvilSeer.Mode", typeof(CrewMate.SeerMode), "Seer.Mode.", translationName: "Seer.Mode")]
     public static CrewMate.SeerMode EvilSeerSeerMode = CrewMate.SeerMode.Both;
+
 
     [CustomOptionBool("EvilSeer.LimitSoulDuration", false, translationName: "Seer.LimitSoulDuration")]
     public static bool EvilSeerLimitSoulDuration = false;
@@ -35,6 +49,7 @@ class EvilSeer : RoleBase<EvilSeer>
     [CustomOptionFloat("EvilSeer.SoulDuration", 2.5f, 60f, 2.5f, 30f, translationName: "Seer.SoulDuration", parentFieldName: nameof(EvilSeerLimitSoulDuration))]
     public static float EvilSeerSoulDuration = 30f;
 
-    [CustomOptionSelect("EvilSeer.SeerColorMode", typeof(CrewMate.DeadBodyColorMode), "Seer.SeerColorMode.", translationName: "Seer.SeerColorMode")]
-    public static  CrewMate.DeadBodyColorMode EvilSeerColorMode = CrewMate.DeadBodyColorMode.None;
+
+    [CustomOptionBool("EvilSeer.ShowArrows", true, translationName: "VultureShowArrows")]
+    public static bool EvilSeerShowArrows;
 }
