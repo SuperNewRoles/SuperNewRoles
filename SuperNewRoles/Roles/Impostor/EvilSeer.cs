ce2125f3c9b8f342392a54a8466a9c6e9a015fde
Kurato-Tsukishiro <kurato.snr7@gmail.com>
2025-06-05 16:53:02 +0900

    feat: 死体色を反映するアビリティを選択する設定を追加
    SNR1では個別で設定していたが、設定数が増えてややこしかった為、一括で設定する形にした。
    色繁栄の設定を全てに反映させたくない場合は、バラバラに設定したいというよりも、一つのみ反映させたいという使い方であると考えた為、この方式で問題ないと判断した。
    (又、SNR1とは霊魂表示の仕様が変わっている事も変更理由に含む。)

--- SuperNewRoles/Roles/Impostor/EvilSeer.cs
+++ SuperNewRoles/Roles/Impostor/EvilSeer.cs
@@ -20,9 +20,9 @@ class EvilSeer : RoleBase<EvilSeer>
             Mode = EvilSeerSeerMode,
             LimitSoulDuration = EvilSeerLimitSoulDuration,
             SoulDuration = EvilSeerSoulDuration,
-            ColorMode = EvilSeerColorMode
+            ColorMode = EvilSeerAbilityColorModeSelect is EvilSeerAbility.All or EvilSeerAbility.SoulOnly ? EvilSeerColorMode : CrewMate.DeadBodyColorMode.None
         }),
-        () => new DeadBodyArrowsAbility(() => EvilSeerShowArrows, colorMode : EvilSeerColorMode),
+        () => new DeadBodyArrowsAbility(() => EvilSeerShowArrows, colorMode : EvilSeerAbilityColorModeSelect is EvilSeerAbility.All or EvilSeerAbility.ArrowOnly ? EvilSeerColorMode : CrewMate.DeadBodyColorMode.None),
     ];
     public override QuoteMod QuoteMod { get; } = QuoteMod.SuperNewRoles;
     public override RoleTypes IntroSoundType { get; } = RoleTypes.Phantom;
@@ -37,6 +37,8 @@ class EvilSeer : RoleBase<EvilSeer>
 
     [CustomOptionSelect("EvilSeer.SeerColorMode", typeof(CrewMate.DeadBodyColorMode), "Seer.SeerColorMode.", translationName: "Seer.SeerColorMode")]
     public static CrewMate.DeadBodyColorMode EvilSeerColorMode = CrewMate.DeadBodyColorMode.None;
+    [CustomOptionSelect("EvilSeer.AbilityColorModeSelect", typeof(EvilSeerAbility), "EvilSeer.AbilityColorModeSelect.", translationName: "EvilSeer.AbilityColorModeSelect", parentFieldName: nameof(EvilSeerColorMode))]
+    public static EvilSeerAbility EvilSeerAbilityColorModeSelect = EvilSeerAbility.SoulOnly;
 
 
     [CustomOptionSelect("EvilSeer.Mode", typeof(CrewMate.SeerMode), "Seer.Mode.", translationName: "Seer.Mode")]
@@ -52,4 +54,11 @@ class EvilSeer : RoleBase<EvilSeer>
 
     [CustomOptionBool("EvilSeer.ShowArrows", true, translationName: "VultureShowArrows")]
     public static bool EvilSeerShowArrows;
+}
+
+public enum EvilSeerAbility
+{
+    All,
+    SoulOnly,
+    ArrowOnly
 }
