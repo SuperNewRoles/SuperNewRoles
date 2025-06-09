646d539e8f1aceb84dda1d85046940142b87f67b
ykundesu <49261069+ykundesu@users.noreply.github.com>
2025-06-09 20:22:46 +0900

    fix: イベントリスナーでエラーが発生した際、他の実行に影響が出ないように対策

--- SuperNewRoles/Roles/Ability/BlackHatHackerAbility.cs
+++ SuperNewRoles/Roles/Ability/BlackHatHackerAbility.cs
@@ -122,6 +122,7 @@ public class BlackHatHackerAbility : AbilityBase
     private void OnFixedUpdate()
     {
         if (!Player.AmOwner) return;
+        if (ExPlayerControl.LocalPlayer == null) return;
 
         // 共有タイマーの更新
         SharedTimer -= Time.fixedDeltaTime;
@@ -135,6 +136,7 @@ public class BlackHatHackerAbility : AbilityBase
         if (IsAllInfected() && (Data.CanDeadWin || !ExPlayerControl.LocalPlayer.IsDead()))
         {
             // BlackHatHacker勝利
+            if (BlackHatHacker.Instance == null) return;
             EndGamer.RpcEndGameWithWinner(
                 CustomGameOverReason.BlackHatHackerWin,
                 WinType.SingleNeutral,
@@ -167,6 +169,8 @@ public class BlackHatHackerAbility : AbilityBase
         {
             PlayerControl player = GameData.Instance.GetPlayerById(id)?.Object;
             if (!player || player.Data.IsDead) continue;
+
+            if (GameManager.Instance == null || GameManager.Instance.LogicOptions == null) continue;
 
             float scope = GameManager.Instance.LogicOptions.GetKillDistance();
             var infection = PlayerControl.AllPlayerControls.ToArray()
