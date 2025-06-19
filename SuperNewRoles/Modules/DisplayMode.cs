b56e99d81f54900b833ccbcc25bddfdb80af3c9c
Kurato-Tsukishiro <kurato.snr7@gmail.com>
2025-06-19 17:05:56 +0900

    add: Mode設定でSHRモードは未実装である表示を追加した。

--- SuperNewRoles/Modules/DisplayMode.cs
+++ SuperNewRoles/Modules/DisplayMode.cs
@@ -6,12 +6,14 @@ namespace SuperNewRoles.Modules;
 public enum DisplayModeId
 {
     Default = 1 << 0,
-    BattleRoyal = 1 << 1,
-    All = Default | BattleRoyal
+    SuperHostRolesOnry = 1 << 1, // 元々の SHR true とは異なる挙動(通常モード 非表示)を目的に使用している。 ( SHRモードに設定された時のみ、「SHR 未実装」と 表記するのに使用中。)
+    BattleRoyal = 1 << 2,
+    All = Default | SuperHostRolesOnry | BattleRoyal
 }
 public enum ModeId
 {
     Default,
+    SuperHostRoles
     //BattleRoyal,
 }
 
@@ -28,10 +30,20 @@ public static class DisplayMode
         {
             case ModeId.Default:
                 return DisplayModeId.Default;
+            case ModeId.SuperHostRoles: // FIXME SHR実装時 想定通りの挙動をしない記述になっている
+                return DisplayModeId.SuperHostRolesOnry;
             //case ModeId.BattleRoyal:
             //    return DisplayModeId.BattleRoyal;
             default:
                 return DisplayModeId.Default;
         }
     }
+}
+
+public static class DisplayCanNotUseSHR // TODO SHR実装時 削除
+{
+    [CustomOptionSelect("SuperHostRoles", typeof(DisplayText), "DisplayText.", parentFieldName: nameof(Categories.ModeOption), displayMode: DisplayModeId.SuperHostRolesOnry, parentActiveValue: ModeId.SuperHostRoles)]
+    public static DisplayText CanNotUseSHR;
+
+    public enum DisplayText { CanNotUseSHR }
 }
