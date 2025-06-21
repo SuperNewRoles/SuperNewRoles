d311544c7e82398e094481e3f7d97a10dc205ea3
Kurato-Tsukishiro <kurato.snr7@gmail.com>
2025-06-21 22:29:38 +0900

    feat: モディファイア狼付きの "DoNotAssignRoles" を設定
    ``IsMadRole()``や``HasAbility(nameof(MadmateAbility))``等からのマッド役職及びフレンズ役職は、Player存在下でないと使用できなかった。
    その為、基本的にマッド・フレンズ役職はRoleIdに"Mad", "Friends" とつくことを利用し、RoleId列挙型から取得するようにした。
    黒猫(BlaclCat)等、当てはまらない役職は手動での追加が必要。

--- SuperNewRoles/Roles/Modifiers/ModifierHauntedWolf.cs
+++ SuperNewRoles/Roles/Modifiers/ModifierHauntedWolf.cs
@@ -26,7 +26,29 @@ class ModifierHauntedWolf : ModifierBase<ModifierHauntedWolf>
     public override Func<ExPlayerControl, string> ModifierMark => (player) => "{0}" + ModHelpers.Cs(RoleColor, "狼");
     public override bool AssignFilter => ModifierHauntedWolfIsAssignMadAndFriendRoles;
 
-    public override RoleId[] DoNotAssignRoles => [];
+    public override RoleId[] DoNotAssignRoles => CanNotAssignRoles();
+
+    /// <summary>RoleId列挙型から、フィルター対象のマッド役職及びフレンズ役職のRoleIdを取得する</summary>
+    /// <returns>フィルター対象の役職のRoleId配列</returns>
+    static RoleId[] CanNotAssignRoles()
+    {
+        // 基本のフィルター
+        List<RoleId> NotAssignRoles = [RoleId.HauntedWolf];
+
+        // 設定が有効でないなら
+        if (!ModifierHauntedWolfIsAssignMadAndFriendRoles) return [.. NotAssignRoles];
+
+        // RoleId に "Mad" とつく役職を取得する
+        List<RoleId> madRoles = [RoleId.BlackCat, RoleId.Worshiper, RoleId.BlackSanta, RoleId.SatsumaAndImo]; // 特殊な命名は 手動で追加
+        madRoles.AddRange(Enum.GetValues<RoleId>().Where(id => id.ToString().StartsWith("Mad"))); // 列挙型から 自動取得
+
+        List<RoleId> friendRoles = [RoleId.JackalFriends]; // まだJFしかいないので 手動での追加
+
+        NotAssignRoles.AddRange(madRoles); // マッドロールをフィルター配列に追加
+        NotAssignRoles.AddRange(friendRoles); // フレンズロールをフィルター配列に追加
+
+        return [.. NotAssignRoles];
+    }
 
     public readonly static List<RoleId> MadAndFriendRoleId = new();
 
