e5204677dc41e7013e20aab156f54fb34f45365a
ykundesu <49261069+ykundesu@users.noreply.github.com>
2025-08-24 22:21:12 +0900

    Update version to v3.0.0.1w

--- SuperNewRoles/statics.cs
+++ SuperNewRoles/statics.cs
@@ -22,7 +22,7 @@ public static class VersionInfo
     public static string VersionString => Current.ToString() + SnapShotVersion.ToString();
 
     public static bool IsSnapShot => SnapShotVersion != null;
-    public static char? SnapShotVersion = 'u';
+    public static char? SnapShotVersion = 'w';
 
     public static string NewVersion = "";
     public static bool IsUpdate = false;
