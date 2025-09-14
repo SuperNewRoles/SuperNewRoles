d11f1343f38667a0fc57e1d1677bb8d541608398
ykundesu <49261069+ykundesu@users.noreply.github.com>
2025-09-14 16:41:16 +0900

    feat: アナリティクスの強化 - 新しいメトリクスを追加し、データ送信の設定を更新。 - ゲーム終了時にアナリティクスを送信するように修正。

--- SuperNewRoles/statics.cs
+++ SuperNewRoles/statics.cs
@@ -32,7 +32,7 @@ public static class VersionInfo
 public static class SNRURLs
 {
     public const string ReportInGameAgreement = "https://wiki.supernewroles.com/reporting-in-game-terms";
-    public const string AnalyticsURL = "https://analytics.supernewroles.com/";
+    public const string AnalyticsURL = "https://analytics.supernewroles.com/api/v2/";
     public const string SNRCS = "https://cs.supernewroles.com";
     public const string ReportInGameAPI = "https://reports-api.supernewroles.com/api/v2";
     public const string UpdateURL = "https://update.supernewroles.com/";
