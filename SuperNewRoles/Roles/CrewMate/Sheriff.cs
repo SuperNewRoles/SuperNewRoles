59fadc8de406f23c5975528518eaf0cc6c8c16e2
Aisu7 <soma12ito03@gmail.com>
2026-06-21 22:17:47 +0900

    Update Sheriff.cs

--- SuperNewRoles/Roles/CrewMate/Sheriff.cs
+++ SuperNewRoles/Roles/CrewMate/Sheriff.cs
@@ -91,7 +91,7 @@ public class SheriffAbilityData
             canKill = CanKillNeutral;
         else if (target.Role == RoleId.HauntedWolf || target.ModifierRole.HasFlag(ModifierRoleId.ModifierHauntedWolf)) // 第三陣営に付与される事はEvilSeerのAbilityによる付与時のみの為、第三陣営としての判定を優先
             canKill = CanKillImpostor;
-        else if ((target.Role == RoleId.MadKiller && !target.IsImpostor()) || target.IsMadRoles())
+        else if (target.IsMadRoles())
             canKill = CanKillMadRoles;
         else if (target.IsFriendRoles())
             canKill = CanKillFriendRoles;
