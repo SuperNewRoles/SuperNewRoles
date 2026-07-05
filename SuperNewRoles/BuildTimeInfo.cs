7d9cc1e3347ddadfcdecb28a353ad47c7d12ca10
ykundesu <github@yoking.dev>
2026-07-05 23:13:40 +0900

    feat: ビルド時間を追加 - うまく動かない時にビルドがしっかりされてるかを確認するため

---
+++ SuperNewRoles/BuildTimeInfo.cs
@@ -0,0 +1,11 @@
+namespace SuperNewRoles
+{
+    internal static partial class BuildTimeInfo
+    {
+#if !GENERATED_BUILD_TIME
+        // ビルド時にGenerated/BuildTime.g.csが生成されていない場合のフォールバック値
+        public const string BuildTimeUtc = "unknown";
+        public const string BuildTimeJst = "unknown";
+#endif
+    }
+}
